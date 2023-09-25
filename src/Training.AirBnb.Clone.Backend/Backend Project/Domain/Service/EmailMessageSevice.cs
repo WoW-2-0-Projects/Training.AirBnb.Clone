61e6b96eb8a56bb848c028b6ae8d115a10b6c529
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-09-25 16:27:20 +0500

    changes done

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Service/EmailMessageSevice.cs
@@ -0,0 +1,12 @@
+﻿using Backend_Project.Domain.Entities;
+using Backend_Project.Domain.Interfaces;
+
+namespace Backend_Project.Domain.Service;
+
+public class EmailMessageSevice : IEmailMessageService<EmailMessage>
+{
+    public ValueTask<EmailMessage> ConvertToMessage(EmailMessage message, Dictionary<string, string> values, string sender, string receiver)
+    {
+        throw new NotImplementedException();
+    }
+}
