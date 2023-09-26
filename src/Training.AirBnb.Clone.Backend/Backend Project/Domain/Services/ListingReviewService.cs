28ff1cb205dea6d4585f6056330085ad53dcedb8
mexroj0804 <mexrojxaybullayev0804@gmal.com>
2023-09-26 17:30:08 +0500

    #32

--- src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Services/ListingReviewService.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Services/ListingReviewService.cs
@@ -6,7 +6,7 @@ using System.Linq.Expressions;
 
 namespace Backend_Project.Domain.Services
 {
-    internal class ListingReviewService : IEntityBaseService<ListingReview>
+    public class ListingReviewService : IEntityBaseService<ListingReview>
     {
         private readonly IDataContext _appDataContext;
 
@@ -18,7 +18,7 @@ namespace Backend_Project.Domain.Services
         public async ValueTask<ListingReview> CreateAsync(ListingReview listingReview, bool saveChanges = true)
         {
             if (string.IsNullOrWhiteSpace(listingReview.Comment) || listingReview.Comment.Length < 1000)
-                throw new ListingReviewFormatException("Invalid commen!");
+                throw new ListingReviewFormatException("Invalid comment!");
             if (listingReview.Rating < 0 || listingReview.Rating > 5)
                 throw new ListingReviewFormatException("Invalid rating!");
             
@@ -49,7 +49,7 @@ namespace Backend_Project.Domain.Services
             var deletedListingReview = await GetById(listingReview.Id);
 
             if (deletedListingReview is null)
-                throw new ListingReviewNotFoundException("Listingreview not found!");
+                throw new ListingReviewNotFoundException("ListingReview not found!");
 
             deletedListingReview.IsDeleted = true;
             deletedListingReview.DeletedDate= DateTime.UtcNow;
@@ -85,7 +85,7 @@ namespace Backend_Project.Domain.Services
             if (updatedListingReview is null)
                 throw new ListingReviewNotFoundException("ListingReview not found!");
             if (string.IsNullOrWhiteSpace(listingReview.Comment) || listingReview.Comment.Length < 1000)
-                throw new ListingReviewFormatException("Invalid commen!");
+                throw new ListingReviewFormatException("Invalid comment!");
             if (listingReview.Rating < 0 || listingReview.Rating > 5)
                 throw new ListingReviewFormatException("Invalid rating!");
 
