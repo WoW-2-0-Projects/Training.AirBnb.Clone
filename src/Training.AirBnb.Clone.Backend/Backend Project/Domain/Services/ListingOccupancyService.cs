cefa810c429b1ed7c716ab353c3e105fcb3b2127
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-09-27 11:08:09 +0500

    implement Interfaces

--- src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Services/ListingOccupancyService.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Services/ListingOccupancyService.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Backend_Project.Domain.Entities;
+using Backend_Project.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Backend_Project.Domain.Services
 {
-    internal class ListingOccupancyService
+    public class ListingOccupancyService : IEntityBaseService<ListingOccupancy>
     {
+        public ValueTask<ListingOccupancy> CreateAsync(ListingOccupancy entity, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<ListingOccupancy> DeleteAsync(Guid id, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<ListingOccupancy> DeleteAsync(ListingOccupancy entity, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<ListingOccupancy> Get(Expression<Func<ListingOccupancy, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<ICollection<ListingOccupancy>> GetAsync(IEnumerable<Guid> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<ListingOccupancy> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<ListingOccupancy> UpdateAsync(ListingOccupancy entity, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
