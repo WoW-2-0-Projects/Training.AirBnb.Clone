8927d25fcd6b761f4e2443d13b82aafc21f5b24b
Ilxom <ilxomkarimjonov22@gmail.com>
2023-09-27 21:04:44 +0500

    create api

---
+++ src/Training.AirBnb.Clone.Backend/AirBnb.Api/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Backend_Project.Domain.Entities;
+using Backend_Project.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirBnb.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IEntityBaseService<User> _userService;
+
+    public UsersController(IEntityBaseService<User> userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("user")]
+    public async Task<IActionResult> PostAsync(User user)
+    {
+        return Ok(await _userService.CreateAsync(user));
+    }
+
+    [HttpGet("user/id")]
+    public async Task<IActionResult> GetByIdAsync(Guid id)
+    {
+        return Ok(await _userService.GetByIdAsync(id));
+    }
+
+    [HttpPut("user/{id}")]
+    public async Task<IActionResult> UpdateAsync(Guid id, User user)
+    {
+        return Ok(await _userService.UpdateAsync(user));
+    }
+    [HttpDelete("user/id")]
+    public async Task<IActionResult> DeleteAsync(Guid id) => Ok(await _userService.DeleteAsync(id));
+}
--- src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Common/AuditableEntity.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Common/AuditableEntity.cs
@@ -1,7 +1,11 @@
-﻿namespace Backend_Project.Domain.Common;
+﻿using System.Text.Json.Serialization;
+
+namespace Backend_Project.Domain.Common;
 
 public abstract class AuditableEntity : Entity, IAuditableEntity
 {
+    [JsonIgnore]
     public DateTimeOffset CreatedDate { get; set; }
+    [JsonIgnore]
     public DateTimeOffset? ModifiedDate { get; set; }
 }
