a8165c0f7128162f829859305854198686780704
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-09-27 11:03:01 +0500

    commit

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/ListingOccupancy.cs
@@ -0,0 +1,21 @@
+﻿using Backend_Project.Domain.Common;
+
+namespace Backend_Project.Domain.Entities
+{
+    public class ListingOccupancy : SoftDeletedEntity
+    {
+        public int Adults { get; set; }
+        public int Children { get; set; }
+        public int Infants {  get; set; }
+        public int Pets { get; set; }
+        public ListingOccupancy(int adults, int children,int infats, int pets)
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTimeOffset.UtcNow;
+            Adults = adults;
+            Children = children;
+            Infants = infats;
+            Pets = pets;
+        }
+    }
+}
