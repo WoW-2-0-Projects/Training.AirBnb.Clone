3b43f41d589bce8195fdf9b25456d43c95687d0b
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-09-29 14:28:21 +0500

    #30: Checking out feature/#19 Stashing uncommitted changes before performing a checkout of 'feature/#19'. Pop or apply these changes to restore them.

--- src/Training.AirBnb.Clone.Backend/Backend Project/Persistance/DataContexts/IDataContext.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Persistance/DataContexts/IDataContext.cs
@@ -1,5 +1,6 @@
 using Backend_Project.Domain.Entities;
 using FileBaseContext.Abstractions.Models.FileSet;
+
 namespace Backend_Project.Persistance.DataContexts;
 
 public interface IDataContext : IAsyncDisposable
