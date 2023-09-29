7f85b2303fd30030a4e06dd3224dc30607fe52f4
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-09-29 13:39:35 +0500

    Added EmailPlaceholderService to Service

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Interfaces/IEmailPlaceholderService.cs
@@ -0,0 +1,8 @@
+﻿using Backend_Project.Domain.Entities;
+
+namespace Backend_Project.Domain.Interfaces;
+
+public interface IEmailPlaceholderService
+{
+    ValueTask<Dictionary<string, string>> GEtTemplateValues(Guid userId,EmailTemplate emailTemplate);
+}
