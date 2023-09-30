0b1f07a74a6ac2f4aaa4f420b404e46fc7fb4048
Ilxom <ilxomkarimjonov22@gmail.com>
2023-09-30 19:14:00 +0500

    Include ListingAmenities Model into data context

--- src/Training.AirBnb.Clone.Backend/Backend Project/Persistance/DataContexts/AppFileContext.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Persistance/DataContexts/AppFileContext.cs
@@ -23,6 +23,7 @@ public class AppFileContext : FileContext, IDataContext
     public IFileSet<ListingCategory,Guid> ListingCategories => Set<ListingCategory>(nameof(ListingCategories));
     public IFileSet<ListingRating, Guid> ListingRatings => Set<ListingRating>(nameof(ListingRatings));
     public IFileSet<Amenity, Guid> Amenities => Set<Amenity>(nameof(Amenities));
+    public IFileSet<ListingAmenities, Guid> ListingAmenities => Set<ListingAmenities>(nameof(ListingAmenities));
 
     public AppFileContext(IFileContextOptions<IFileContext> fileContextOptions) : base(fileContextOptions)
     {
