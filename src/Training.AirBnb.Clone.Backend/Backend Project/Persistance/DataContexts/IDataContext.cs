6f78e840d4841322bad1b623e83883b340f27946
Ilxom <ilxomkarimjonov22@gmail.com>
2023-09-30 20:34:08 +0500

    Add ListingOccupancy model into file context

--- src/Training.AirBnb.Clone.Backend/Backend Project/Persistance/DataContexts/IDataContext.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Persistance/DataContexts/IDataContext.cs
@@ -19,6 +19,7 @@ public interface IDataContext : IAsyncDisposable
     IFileSet<AmenityCategory, Guid> AmenityCategories { get; }
     IFileSet<ListingRating, Guid> ListingRatings { get; }
     IFileSet<Amenity, Guid> Amenities { get; }
+    IFileSet<ListingOccupancy, Guid> ListingOccupancies { get; }
 
     ValueTask SaveChangesAsync();
 }
