35034f147bfd40767432562a03109dd85c34efa9
Ilxom <ilxomkarimjonov22@gmail.com>
2023-09-30 19:10:12 +0500

    Add ListingAmenities Model

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/ListingAmenities.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend_Project.Domain.Entities;
+
+public class ListingAmenities
+{
+    public Guid ListingId { get; set; }
+    public Guid AmenityId { get; set; }
+
+    public ListingAmenities(Guid listingId, Guid amenityId)
+    {
+        ListingId = listingId;
+        AmenityId = amenityId;
+    }
+}
