c438b0a9e37fcf3cbdfd2ac379be546bcaecbe4c
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-02 18:10:33 +0500

    Create Listing category feature option

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/ListingCategoryFeatureOption.cs
@@ -0,0 +1,23 @@
+﻿using Backend_Project.Domain.Common;
+
+namespace Backend_Project.Domain.Entities;
+
+public class ListingCategoryFeatureOption : SoftDeletedEntity
+{
+    public Guid ListingCategoryId { get; set; }
+    public Guid ListingFeatureOptionId { get; set; } 
+    public Guid ListingFeatureId { get; set; }
+    public int FeatureMinValue { get; set; }   
+    public int FeatureMaxValue { get; set; }   
+
+    public ListingCategoryFeatureOption(Guid listingCategoryId, Guid listingFeatureOptionId, Guid listingFeatureId, int featureMinValue, int featureMaxValue)
+    {
+        Id = Guid.NewGuid();
+        ListingCategoryId = listingCategoryId;
+        ListingFeatureOptionId = listingFeatureOptionId;
+        ListingFeatureId = listingFeatureId;
+        FeatureMinValue = featureMinValue;
+        FeatureMaxValue = featureMaxValue;
+        CreatedDate = DateTimeOffset.UtcNow;
+    }
+}
