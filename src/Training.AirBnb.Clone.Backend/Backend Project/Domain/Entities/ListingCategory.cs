92729da1fe35dd2dabf98dc166ca995cdf2947e8
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-04 07:14:24 +0500

    Remove constructors and prevent nullable warnings from models

--- src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/ListingCategory.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/ListingCategory.cs
@@ -1,23 +1,11 @@
-﻿using Backend_Project.Domain.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#pragma warning disable CS8618
+
+using Backend_Project.Domain.Common;
 
 namespace Backend_Project.Domain.Entities
 {
     public class ListingCategory : SoftDeletedEntity
     {
         public string Name { get; set; }
-        public ListingCategory(string name)
-        {
-            Name = name;
-            Id = Guid.NewGuid();
-            CreatedDate = DateTime.UtcNow;
-        }
-
-
-
     }
 }
--- src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/ListingFeatureOption.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/ListingFeatureOption.cs
@@ -1,15 +1,10 @@
-﻿using Backend_Project.Domain.Common;
+﻿#pragma warning disable CS8618
+
+using Backend_Project.Domain.Common;
 
 namespace Backend_Project.Domain.Entities;
 
 public class ListingFeatureOption : SoftDeletedEntity
 {
     public string Name { get; set; }
-
-    public ListingFeatureOption(string name)
-    {
-        Id = Guid.NewGuid();
-        Name = name;
-        CreatedDate = DateTime.UtcNow;
-    }
 }
