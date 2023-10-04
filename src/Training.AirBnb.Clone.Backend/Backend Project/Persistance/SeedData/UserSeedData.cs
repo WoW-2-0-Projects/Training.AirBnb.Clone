0308d4b55281ac1b2ced8e7b91a14094b127b509
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-10-04 22:07:28 +0500

    Cresated UserSeed datas

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Persistance/SeedData/UserSeedData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Project.Persistance.SeedData
+{
+    internal class UserSeedData
+    {
+    }
+}
