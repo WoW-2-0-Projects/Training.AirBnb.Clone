a1bb0b1891b48a207414f69962a83df430ed1864
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-10-04 10:02:11 +0500

    Auto stash before rebase of "origin/dev"

--- src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/AmenityCategory.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/AmenityCategory.cs
@@ -1,10 +1,8 @@
-﻿#pragma warning disable CS8618
+#pragma warning disable CS8618
 
 using Backend_Project.Domain.Common;
 
 namespace Backend_Project.Domain.Entities;
 
-public class AmenityCategory : SoftDeletedEntity
 {
-    public string CategoryName { get; set; }
 }
