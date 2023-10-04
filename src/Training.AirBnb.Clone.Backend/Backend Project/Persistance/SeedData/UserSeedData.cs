f65cd6aa16e55b60040afa1d1d78f6279acd3414
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-10-04 22:12:55 +0500

    done

--- src/Training.AirBnb.Clone.Backend/Backend Project/Persistance/SeedData/UserSeedData.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Persistance/SeedData/UserSeedData.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿using Backend_Project.Domain.Common;
+using Backend_Project.Domain.Entities;
+using Backend_Project.Domain.Enums;
+using Backend_Project.Persistance.DataContexts;
+using Bogus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Backend_Project.Persistance.SeedData
+namespace Backend_Project.Persistance.SeedData;
+
+public static class UserSeedData
 {
-    internal class UserSeedData
+    public static async ValueTask InitializeSeedDataAsync(this IDataContext fileContext)
     {
+        var admin =fileContext.GetUserSystem();
+        if (!fileContext.Users.Any())
+        {
+            await fileContext.Users.AddAsync(admin);
+            await fileContext.AddAsync<User>(100);
+            
+        }
+
+        if (!fileContext.UserCredentials.Any())
+        {
+            var userCredential = new UserCredentials { CreatedDate=DateTime.Now,UserId=admin.Id,Password="Aa1234!@a" };
+            await fileContext.UserCredentials.AddAsync(userCredential);
+            await fileContext.AddAsync<UserCredentials>(100);
+
+        }
+    }
+    public static User GetUserSystem(this IDataContext fileContext)=> new User { FirstName = "System", LastName = "Project", UserRole = UserRole.Admin, EmailAddress = "system@gmail.com" };
+
+     public static async ValueTask AddAsync<TEntity>(this IDataContext context, int count) where TEntity : IEntity
+    {
+        var task = typeof(TEntity) switch
+        {
+            { } t when t == typeof(User) => context.AddUsersAsync(count),
+            { } t when t == typeof(UserCredentials) => context.AddUserCredentials(count),
+            _ => new ValueTask(Task.CompletedTask)
+        };
+    }
+    public static async ValueTask AddUsersAsync(this IDataContext context, int count)
+    {
+        var faker = GetUserFaker(context);
+        var uniqueUsers = new HashSet<User>(faker.Generate(100_000));
+        var test = uniqueUsers.Take(count);
+        await context.Users.AddRangeAsync(uniqueUsers.Take(count).ToList());
+    }
+    public static async ValueTask AddUserCredentials(this IDataContext context, int count)
+    {
+        var faker = GetUserCredentialsFaker(context);
+        var userCredentials = faker.Generate(context.Users.Count());
+        await context.UserCredentials.AddRangeAsync(userCredentials.Take(count).ToList());
+    }
+    public static Faker<User> GetUserFaker(IDataContext context)
+    {
+        return new Faker<User>()
+            .RuleFor(keySelector => keySelector.Id, Guid.NewGuid)
+            .RuleFor(keySelector => keySelector.FirstName, source => source.Person.FirstName)
+            .RuleFor(keySelector => keySelector.LastName, source => source.Person.LastName)
+            .RuleFor(keySelector => keySelector.EmailAddress, source => source.Person.Email);
+    }
+
+    public static Faker<UserCredentials> GetUserCredentialsFaker(IDataContext context)
+    {
+        var usersId = new Stack<Guid>(context.Users.Select(user => user.Id));
+        return new Faker<UserCredentials>()
+            .RuleFor(keySelector => keySelector.Id, Guid.NewGuid)
+            .RuleFor(keySelector => keySelector.UserId, () => usersId.Pop())
+            .RuleFor(keySelector => keySelector.Password, source => source.Internet.Password(8, false, "", "1234567890"));
     }
 }
