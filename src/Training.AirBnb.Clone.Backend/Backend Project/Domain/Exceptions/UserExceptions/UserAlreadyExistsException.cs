ed74559fd072cc2ee864781b55aa8695f0d6242a
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-10-05 11:46:21 +0500

    some of the unnecessary file added to the project has been deleted. Errors were cleared

--- src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Exceptions/UserExceptions/UserAlreadyExistsException.cs
+++
@@ -1,7 +0,0 @@
-namespace Backend_Project.Domain.Exceptions.User;
-
-public class UserAlreadyExistsException:Exception
-{
-    public UserAlreadyExistsException() { } 
-    public UserAlreadyExistsException(string message) : base(message){}
-}
--- src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Interfaces/IValidationService.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Interfaces/IValidationService.cs
@@ -1,11 +1,7 @@
-    bool IsValidEmailAddress(string emailAddress);
 namespace Backend_Project.Domain.Interfaces;
 
 public interface IValidationService
 {
     ValueTask<bool> IsValidNameAsync(string name);
-
     bool IsValidEmailAddress(string emailAddress);
-
-    bool IsValidPhoneNumber(string phoneNumber);
 }
