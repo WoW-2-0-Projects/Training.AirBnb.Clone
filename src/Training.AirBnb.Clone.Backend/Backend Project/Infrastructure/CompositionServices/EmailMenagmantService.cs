3e3d55d14b11ebbb0f6ff18a8cd4a75b90b92b3f
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-10-05 11:55:10 +0500

    update userFound field

--- src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/CompositionServices/EmailMenagmantService.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/CompositionServices/EmailMenagmantService.cs
@@ -37,7 +37,9 @@ namespace Backend_Project.Infrastructure.CompositionServices
 
             var placeholders = await _emailPlaceholderService.GetTemplateValues(userId, template);
 
-            var founduser = await _userService.GetByIdAsync(userId) ?? throw new EntityException<User>("User Not found");
+            var foundUser = await _userService.GetByIdAsync(userId);
+            if (foundUser is null)
+                throw new EntityException<User>("User Not found");
 
             var message = await _emailMessageService.ConvertToMessage(template, placeholders, "sultonbek.rakhimov.recovery@gmail.com", "asadbekrashidov000gmail.com");
 
