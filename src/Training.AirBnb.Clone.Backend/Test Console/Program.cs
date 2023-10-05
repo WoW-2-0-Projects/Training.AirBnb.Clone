abc33a708ae9e60fe36bbf48626c657955d7c312
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-05 18:42:43 +0500

    Refactor program files

--- src/Training.AirBnb.Clone.Backend/Test Console/Program.cs
+++ src/Training.AirBnb.Clone.Backend/Test Console/Program.cs
@@ -1,9 +1,1 @@
-using Backend_Project.Domain.Entities;
-using Backend_Project.Infrastructure.Services.LocationServices;
-using Backend_Project.Persistance.DataContexts;
-using FileBaseContext.Context.Models.Configurations;
-
-var contextOptions = new FileContextOptions<AppFileContext>();
-var context = new AppFileContext(contextOptions);
-context.FetchAsync().AsTask().Wait();
-
+Console.WriteLine("Hello world.");
