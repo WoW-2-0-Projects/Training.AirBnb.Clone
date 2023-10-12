05f9fa3d2ef8f132093f0b476c2066e390639031
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-10-12 13:55:00 +0500

    Update EmailMessageService

--- src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/Services/NotificationsServices/EmailMessageSevice.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/Services/NotificationsServices/EmailMessageSevice.cs
@@ -5,17 +5,30 @@ namespace Backend_Project.Infrastructure.Services.NotificationsServices;
 
 public class EmailMessageSevice : IEmailMessageService
 {
-    public ValueTask<EmailMessage> ConvertToMessage(EmailTemplate emailTemplate, Dictionary<string, string> values, string sender, string receiver)
+    private readonly IEntityBaseService<User> _userService;
+
+    public EmailMessageSevice(IEntityBaseService<User> userService)
     {
-       var body = emailTemplate.Body;
+        _userService = userService;
+    }
+
+    public async ValueTask<EmailMessage> ConvertToMessage(EmailTemplate emailTemplate, Dictionary<string, string> values, Guid senderUserId, Guid receiverUserId)
+    {
+        var senderUser = await _userService.GetByIdAsync(senderUserId);
+        var receiverUser = await _userService.GetByIdAsync(receiverUserId);
+
+        var body = emailTemplate.Body;
+        var subject = emailTemplate.Subject;
 
         foreach (var item in values)
         {
             body = body.Replace(item.Key, item.Value);
+            subject = subject.Replace(item.Key, item.Value);
         }
 
-        var EmailMessage = new EmailMessage(emailTemplate.Subject, body, sender, receiver);
+        var emailMessage = new EmailMessage(subject, body, senderUserId, receiverUserId, senderUser.EmailAddress, receiverUser.EmailAddress);
 
-        return ValueTask.FromResult(EmailMessage);
+        return emailMessage;
+
     }
 }
