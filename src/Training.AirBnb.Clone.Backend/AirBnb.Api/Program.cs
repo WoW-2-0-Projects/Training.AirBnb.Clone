e3ad5e340e4f22c24e16aba7a412f0837b1b5d03
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-15 13:33:54 +0500

    Apply Host Configuration in Program.cs to keep it clean

--- src/Training.AirBnb.Clone.Backend/AirBnb.Api/Program.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnb.Api/Program.cs
@@ -1,76 +1,11 @@
-using Backend_Project.Application.Interfaces;
-using Backend_Project.Domain.Entities;
-using Backend_Project.Infrastructure.CompositionServices;
-using Backend_Project.Infrastructure.Services;
-using Backend_Project.Infrastructure.Services.AccountServices;
-using Backend_Project.Infrastructure.Services.ListingServices;
-using Backend_Project.Infrastructure.Services.NotificationsServices;
-using Backend_Project.Persistance.DataContexts;
-using Backend_Project.Persistance.SeedData;
-using FileBaseContext.Context.Models.Configurations;
+using AirBnb.Api.Configs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<IDataContext, AppFileContext>(_ =>
-{
-    var contextOptions = new FileContextOptions<AppFileContext>
-    {
-        StorageRootPath = Path.Combine(builder.Environment.ContentRootPath, "Data", "DataStorage")
-    };
-
-    var context = new AppFileContext(contextOptions);
-    context.FetchAsync().AsTask().Wait();
-
-    return context;
-});
-
-builder.Services.AddScoped<IEntityBaseService<User>, UserService>();
-builder.Services.AddSingleton<IValidationService, ValidationService>();
-builder.Services.AddScoped<IListingCategoryDetailsService, ListingCategoryDetailsService>();
-builder.Services.AddScoped<IEntityBaseService<Amenity>, AmenityService>();
-builder.Services.AddScoped<IEntityBaseService<ListingCategory>, ListingCategoryService>();
-builder.Services.AddScoped<IEntityBaseService<ListingFeature>, ListingFeatureService>();
-builder.Services.AddScoped<IEntityBaseService<ListingType>, ListingTypeService>();
-builder.Services.AddScoped<IEntityBaseService<ListingCategoryType>, ListingCategoryTypeService>();
-builder.Services.AddScoped<IEntityBaseService<Listing>, ListingService>();
-
-builder.Services.AddScoped<IEntityBaseService<AmenityCategory>, AmenityCategoryService>();
-builder.Services.AddScoped<IEntityBaseService<ListingAmenities>, ListingAmenitiesService>();
-builder.Services.AddScoped<IAmenitiesManagementService, AmenitiesManagementService>();
-
-builder.Services.AddScoped<IEntityBaseService<ListingProperty>, ListingPropertyService>();
-builder.Services.AddScoped<IEntityBaseService<Email>, EmailService>();
-builder.Services.AddScoped<IEntityBaseService<EmailTemplate>, EmailTemplateService>();
-
-builder.Services.AddScoped<IEmailMenegmentService,EmailMenagmantService>();
-builder.Services.AddScoped<IEmailPlaceholderService, EmailPlaceholderService>();
-builder.Services.AddScoped<IEmailSenderService, EmailSenderService>();
-builder.Services.AddScoped<IEmailMessageService, EmailMessageSevice>();
-
+builder.Configure();
 
 var app = builder.Build();
 
-var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<IDataContext>();
-await context.InitializeCategoryDetailsSeedData();
+await app.ConfigureAsync();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await app.RunAsync();
