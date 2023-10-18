c62a7ef65e11fecbc116c0a221d333a23b39629a
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-10-18 18:51:38 +0500

    for Merge dev

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Persistence/SeedData/ListingPropertyTypeSeedData.cs
@@ -0,0 +1,11 @@
+﻿using Backend_Project.Persistence.DataContexts;
+
+namespace Backend_Project.Persistence.SeedData;
+
+public static class ListingPropertyTypeSeedData
+{
+    public static async ValueTask InitializeListingProperyTypeSeedData(this IDataContext context)
+    {
+        //if(context.ListingPropertyTypes)
+    }
+}
