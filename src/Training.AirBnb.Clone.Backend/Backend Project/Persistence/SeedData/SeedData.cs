0ff7eb2cc769aa8bbaf9d72ed77096c8e65281cf
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-10-19 10:12:40 +0500

    Move InitializeListingproperty seedata to Generic Seed data

--- src/Training.AirBnb.Clone.Backend/Backend Project/Persistence/SeedData/SeedData.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Persistence/SeedData/SeedData.cs
@@ -1,24 +1,37 @@
-﻿using Backend_Project.Domain.Entities;
+﻿using Backend_Project.Domain.Common;
+using Backend_Project.Domain.Entities;
 using Backend_Project.Domain.Enums;
 using Backend_Project.Persistence.DataContexts;
 
 namespace Backend_Project.Persistence.SeedData;
 
-public static class ListingPropertyTypeSeedData
+public static class SeedData
 {
-    public static async ValueTask InitializeListingProperyTypeSeedData(this IDataContext context)
+    public static async ValueTask InitializeSeedDataAsync(this IDataContext context)
     {
         if (!context.ListingPropertyTypes.Any())
-            await context.InitializeListingProperyType();
+            await context.AddSeedDataAsync<ListingPropertyType>(1000);
     }
 
-    private static async ValueTask InitializeListingProperyType(this IDataContext context)
+    private static async ValueTask AddSeedDataAsync<TEntity>(this IDataContext context, int count = 0) where TEntity : IEntity
+    {
+        var task = typeof(TEntity) switch
+        {
+            { } type when type == typeof(ListingPropertyType) => context.AddListingProperyType(count),
+            _ => new ValueTask(Task.CompletedTask)
+        };
+
+        await task;
+        await context.SaveChangesAsync();
+    }
+
+    private static async ValueTask AddListingProperyType(this IDataContext context, int count)
     {
         var listingPropertyTypes = new List<ListingPropertyType>();
 
         var random = new Random();
 
-        for(int index = 0; index < 1000; index++)
+        for (int index = 0; index < count; index++)
         {
             var floorsCount = random.Next(1, 180);
 
@@ -31,12 +44,11 @@ public static class ListingPropertyTypeSeedData
                 FloorsCount = floorsCount,
                 ListingFloor = random.Next(1, floorsCount),
                 YearBuilt = random.Next(1900, DateTime.UtcNow.Year),
-                PropertySize = random.Next(1,1_000_000_000),
-                UnitOfSize = floorsCount % 2 == 0 ?  UnitsOfSize.SquareMetres : UnitsOfSize.SquareFeet                
+                PropertySize = random.Next(1, 1_000_000_000),
+                UnitOfSize = floorsCount % 2 == 0 ? UnitsOfSize.SquareMetres : UnitsOfSize.SquareFeet
             });
         }
 
         await context.ListingPropertyTypes.AddRangeAsync(listingPropertyTypes);
-        await context.SaveChangesAsync();
     }
 }
