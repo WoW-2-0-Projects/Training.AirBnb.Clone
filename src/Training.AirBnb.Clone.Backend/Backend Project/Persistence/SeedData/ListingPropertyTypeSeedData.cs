132c7aa65244ef38427780a328c04b4c3abb14a5
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-10-19 08:54:58 +0500

    add different type Exception messages

--- src/Training.AirBnb.Clone.Backend/Backend Project/Persistence/SeedData/ListingPropertyTypeSeedData.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Persistence/SeedData/ListingPropertyTypeSeedData.cs
@@ -1,4 +1,5 @@
-﻿using Backend_Project.Persistence.DataContexts;
+﻿using Backend_Project.Domain.Entities;
+using Backend_Project.Persistence.DataContexts;
 
 namespace Backend_Project.Persistence.SeedData;
 
@@ -6,6 +7,30 @@ public static class ListingPropertyTypeSeedData
 {
     public static async ValueTask InitializeListingProperyTypeSeedData(this IDataContext context)
     {
-        //if(context.ListingPropertyTypes)
+        if (!context.ListingPropertyTypes.Any())
+            await context.InitializeListingProperyType();
+    }
+
+    private static async ValueTask InitializeListingProperyType(this IDataContext context)
+    {
+        var listingPropertyTypes = new List<ListingPropertyType>();
+
+        var listingCategories = context.ListingCategories.ToList();
+        var listingTypes = context.ListingTypes.ToList();
+
+        var random = new Random();
+
+        for(int index = 0; index < 1000; index++)
+        {
+            listingPropertyTypes.Add(new ListingPropertyType()
+            {
+               // CategoryId = listingCategories[random.Next(0, listingCategories.Count())],
+               // TypeId = listingCategoryTypes[random.Next(0, context.ListingTypes.)]
+
+            });
+        }
+
+        await context.ListingPropertyTypes.AddRangeAsync(listingPropertyTypes);
+        await context.SaveChangesAsync();
     }
 }
