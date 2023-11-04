760a422a988d13ad843279a76ea6d857d2e6ea46
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-04 16:17:39 +0500

    Create listing registration dto models

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Application/Location/Dtos/AddressDto.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable CS8618
+
+namespace Backend_Project.Application.Location.Dtos;
+
+public class AddressDto
+{
+    public Guid? CityId { get; set; }
+    public string AddressLine1 { get; set; }
+    public string? AddressLine2 { get; set; }
+    public string? AddressLine3 { get; set; }
+    public string? AddressLine4 { get; set; }
+    public string? Province { get; set; }
+    public string? ZipCode { get; set; }
+}
