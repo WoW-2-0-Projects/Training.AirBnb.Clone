a18ebbed8f131e341b5920ca19f63c360d1d2a4b
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-04 16:20:53 +0500

    Create listing registration service and Create Listing method

--- src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/ListingRegistrationProgress.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Domain/Entities/ListingRegistrationProgress.cs
@@ -7,4 +7,10 @@ public class ListingRegistrationProgress : SoftDeletedEntity
     public int Progress { get; set; }
 
     public Guid ListingId { get; set; }
+
+    public ListingRegistrationProgress(int progress, Guid listingId)
+    {
+        Progress = progress;
+        ListingId = listingId;
+    }
 }
