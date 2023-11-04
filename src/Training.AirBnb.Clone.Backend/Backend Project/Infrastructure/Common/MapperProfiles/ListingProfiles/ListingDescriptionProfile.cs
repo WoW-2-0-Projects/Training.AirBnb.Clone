b6b26e5c2965bf6ecb36068221eaf19b625069dc
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-04 16:20:04 +0500

    Add mapping profiles for listing and location models

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/Common/MapperProfiles/ListingProfiles/ListingDescriptionProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Backend_Project.Application.Listings.Dtos;
+using Backend_Project.Domain.Entities;
+
+namespace Backend_Project.Infrastructure.Common.MapperProfiles.ListingProfiles;
+
+public class ListingDescriptionProfile : Profile
+{
+    public ListingDescriptionProfile()
+    {
+        CreateMap<Description, ListingDescriptionRegistrationDto>();
+        CreateMap<ListingDescriptionRegistrationDto, Description>();
+    }
+}
---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/Common/MapperProfiles/LocationProfiles/AddressProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Backend_Project.Application.Location.Dtos;
+using Backend_Project.Domain.Entities;
+
+namespace Backend_Project.Infrastructure.Common.MapperProfiles.LocationProfiles;
+
+public class AddressProfile : Profile
+{
+    public AddressProfile()
+    {
+        CreateMap<Address, AddressDto>();
+        CreateMap<AddressDto, Address>();
+    }
+}
