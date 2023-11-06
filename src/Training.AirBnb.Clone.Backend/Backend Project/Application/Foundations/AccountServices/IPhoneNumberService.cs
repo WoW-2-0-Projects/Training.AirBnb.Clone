42e9b93f7a59c0ed5b60f7fef67d36ae5ed51d12
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-06 21:14:28 +0500

    Refactor services

--- src/Training.AirBnb.Clone.Backend/Backend Project/Application/Foundations/AccountServices/IPhoneNumberService.cs
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Application/Foundations/AccountServices/IPhoneNumberService.cs
@@ -7,15 +7,15 @@ public interface IPhoneNumberService
 {
     IQueryable<PhoneNumber> Get(Expression<Func<PhoneNumber, bool>> predicate);
 
-    ValueTask<ICollection<PhoneNumber>> GetAsync(IEnumerable<Guid> ids, CancellationToken cancellationoken = default);
+    ValueTask<ICollection<PhoneNumber>> GetAsync(IEnumerable<Guid> ids, CancellationToken cancellationToken = default);
 
-    ValueTask<PhoneNumber> GetByIdAsync(Guid id, CancellationToken cancellationoken = default);
+    ValueTask<PhoneNumber> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
 
-    ValueTask<PhoneNumber> CreateAsync(PhoneNumber phoneNumber, bool saveChanges = true, CancellationToken cancellationoken = default);
+    ValueTask<PhoneNumber> CreateAsync(PhoneNumber phoneNumber, bool saveChanges = true, CancellationToken cancellationToken = default);
 
-    ValueTask<PhoneNumber> UpdateAsync(PhoneNumber phoneNumber, bool saveChanges = true, CancellationToken cancellationoken = default);
+    ValueTask<PhoneNumber> UpdateAsync(PhoneNumber phoneNumber, bool saveChanges = true, CancellationToken cancellationToken = default);
 
-    ValueTask<PhoneNumber> DeleteAsync(Guid id, bool saveChanges = true, CancellationToken cancellationoken = default);
+    ValueTask<PhoneNumber> DeleteAsync(Guid id, bool saveChanges = true, CancellationToken cancellationToken = default);
 
-    ValueTask<PhoneNumber> DeleteAsync(PhoneNumber phoneNumber, bool saveChanges = true, CancellationToken cancellationoken = default);
+    ValueTask<PhoneNumber> DeleteAsync(PhoneNumber phoneNumber, bool saveChanges = true, CancellationToken cancellationToken = default);
 }
