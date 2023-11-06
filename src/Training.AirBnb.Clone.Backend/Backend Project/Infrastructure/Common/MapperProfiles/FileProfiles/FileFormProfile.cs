e2973d1935e1ddd55ea2d5593918f6401483a060
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-06 17:44:44 +0500

    Create file and image dtos and enable automapping

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/Common/MapperProfiles/FileProfiles/FileFormProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Backend_Project.Application.Files.Dtos;
+using Microsoft.AspNetCore.Http;
+
+namespace Backend_Project.Infrastructure.Common.MapperProfiles.FileProfiles;
+
+public class FileFormProfile : Profile
+{
+    public FileFormProfile()
+    {
+        CreateMap<IFormFile, UploadFileDto>()
+            .ForMember(dest => dest.Source, opt => opt
+                .MapFrom(src => src.OpenReadStream()))
+            .ForMember(dest => dest.Size, opt => opt
+                .MapFrom(src => src.Length));
+    }
+}
---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/Common/MapperProfiles/FileProfiles/ImageInfoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Backend_Project.Application.Files.Dtos;
+using Backend_Project.Application.Listings.Dtos;
+using Backend_Project.Domain.Entities;
+
+namespace Backend_Project.Infrastructure.Common.MapperProfiles.FileProfiles;
+
+public class ImageInfoProfile : Profile
+{
+    public ImageInfoProfile()
+    {
+        CreateMap<UploadFileDto, ImageInfo>();
+        CreateMap<ImageInfo, ImageInfoDto>();
+    }
+}
