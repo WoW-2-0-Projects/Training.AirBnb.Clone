03e7473b705738badff92c5712c282a523292ce4
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-06 17:24:36 +0500

    Create directory and file brokers

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Application/Files/Brokers/IDirectoryBroker.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend_Project.Application.Files.Brokers;
+
+public interface IDirectoryBroker
+{
+    DirectoryInfo CreateDirectory(string path);
+
+    void DeleteDirectory(string path, bool deleteSubDirectoriesAndFiles = false);
+
+    IEnumerable<string> EnumerateFiles(string path);
+}
