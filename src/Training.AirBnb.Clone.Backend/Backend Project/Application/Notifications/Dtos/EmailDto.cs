b269159dca504f888707c6056279e9dfb7370bc3
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-09 18:47:35 +0500

    Added  EmailDto and EmailTemplateDto

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Application/Notifications/Dtos/EmailDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Backend_Project.Application.Notifications.Dtos;
+
+public class EmailDto
+{
+    public Guid Id { get; set; }
+    
+    public Guid ReceiverUserId { get; set; }
+    
+    public string? Subject { get; set; }
+    
+    public string? Body { get; set;}
+    
+    public string? ReceiverEmailAddress { get; set; }
+    
+    public string? SenderEmailAddress { get; set; }
+    
+    public bool IsSent { get; set; } 
+}
---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Application/Notifications/Dtos/EmailTemplateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend_Project.Application.Notifications.Dtos;
+
+public class EmailTemplateDto
+{
+    public Guid Id { get; set; }
+    
+    public string? Subject { get; set; }
+    
+    public string? Body { get; set; }
+}
