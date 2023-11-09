80628c79bbcf521b18df81b20449d40172912263
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-09 17:10:55 +0500

    Implement listing property type services crud validation

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Application/Listings/Services/IListingPropertiesProcessingService.cs
@@ -0,0 +1,18 @@
+﻿using Backend_Project.Domain.Entities;
+
+namespace Backend_Project.Application.Listings.Services;
+
+public interface IListingPropertiesProcessingService
+{
+    ValueTask<ListingPropertyType> CreateListingPropertyTypeAsync(ListingPropertyType propertyType);
+
+    ValueTask<ListingPropertyType> UpdateListingPropertyTypeAsync(ListingPropertyType propertyType);
+
+    ValueTask<ListingPropertyType> DeleteListingPropertyTypeAsync(ListingPropertyType propertyType);
+
+    ListingProperty CreateListingProperty(ListingProperty property);
+
+    ListingProperty UpdateListingProperty(ListingProperty property);
+
+    ListingProperty DeleteListingProperty(ListingProperty property);
+}
---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/CompositionServices/ListingPropertiesProcessingService.cs
@@ -0,0 +1,88 @@
+﻿using Backend_Project.Application.Foundations.ListingServices;
+using Backend_Project.Application.Listings.Services;
+using Backend_Project.Domain.Entities;
+using Backend_Project.Domain.Exceptions.EntityExceptions;
+
+namespace Backend_Project.Infrastructure.CompositionServices;
+
+public class ListingPropertiesProcessingService : IListingPropertiesProcessingService
+{
+    private readonly IListingPropertyTypeService _propertyTypeService;
+    private readonly IListingPropertyService _propertyService;
+    private readonly IListingService _listingService;
+    private readonly IListingCategoryTypeService _listingCategoryTypeService;
+    private readonly IListingFeatureService _listingFeatureService;
+
+    public ListingPropertiesProcessingService(
+        IListingPropertyTypeService propertyTypeService,
+        IListingPropertyService propertyService,
+        IListingService listingService,
+        IListingCategoryTypeService listingCategoryTypeService,
+        IListingFeatureService listingFeatureService)
+    {
+        _propertyTypeService = propertyTypeService;
+        _propertyService = propertyService;
+        _listingService = listingService;
+        _listingCategoryTypeService = listingCategoryTypeService;
+        _listingFeatureService = listingFeatureService;
+    }
+
+    public async ValueTask<ListingPropertyType> CreateListingPropertyTypeAsync(ListingPropertyType propertyType)
+    {
+        ValidatePropertyType(propertyType);
+
+        var createdPropertyType = await _propertyTypeService.CreateAsync(propertyType);
+
+        return createdPropertyType;
+    }
+
+    public async ValueTask<ListingPropertyType> UpdateListingPropertyTypeAsync(ListingPropertyType propertyType)
+    {
+        ValidatePropertyType(propertyType);
+
+        var updatedPropertyType = await _propertyTypeService.UpdateAsync(propertyType);
+
+        return updatedPropertyType;
+    }
+
+    public async ValueTask<ListingPropertyType> DeleteListingPropertyTypeAsync(ListingPropertyType propertyType)
+    {
+        var listing = _listingService.Get(self => true).FirstOrDefault(self => self.PropertyTypeId == propertyType.Id);
+
+        if (listing is not null)
+            throw new EntityNotDeletableException<ListingPropertyType>("There is an active listing which has this property type.");
+        
+        return await _propertyTypeService.DeleteAsync(propertyType);
+    }
+
+    public ListingProperty CreateListingProperty(ListingProperty property)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ListingProperty UpdateListingProperty(ListingProperty property)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ListingProperty DeleteListingProperty(ListingProperty property)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ValidatePropertyType(ListingPropertyType propertyType)
+    {
+        var categoryTypes = _listingCategoryTypeService
+            .Get(category => true)
+            .Where(category => category.ListingCategoryId == propertyType.CategoryId);
+
+        if (!categoryTypes.Any())
+            throw new EntityNotFoundException<ListingCategory>("Category not found!");
+
+        if (propertyType.TypeId is not null)
+            _ = categoryTypes
+                .FirstOrDefault(categoryType => categoryType.ListingCategoryId == propertyType.CategoryId
+                && categoryType.ListingTypeId == propertyType.TypeId)
+                ?? throw new EntityNotFoundException<ListingType>("Listing type not found!");
+    }
+}
