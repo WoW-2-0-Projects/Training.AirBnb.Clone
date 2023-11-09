c5d6c6f43ebe8c653718dcd7c8848caa23189847
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-09 18:48:10 +0500

    Added EmailProfile and EmailTemplateProfile

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/Common/MapperProfiles/NotificationProfile/EmailProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Backend_Project.Application.Notifications.Dtos;
+using Backend_Project.Domain.Entities;
+
+namespace Backend_Project.Infrastructure.Common.MapperProfiles.NotificationProfile;
+
+public class EmailProfile : Profile
+{
+    public EmailProfile()
+    {
+        CreateMap<Email, EmailDto>();
+        CreateMap<EmailDto, Email>();
+    }
+}
---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/Common/MapperProfiles/NotificationProfile/EmailTemplateProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Backend_Project.Application.Notifications.Dtos;
+using Backend_Project.Domain.Entities;
+
+namespace Backend_Project.Infrastructure.Common.MapperProfiles.NotificationProfile;
+
+public class EmailTemplateProfile : Profile
+{
+    public EmailTemplateProfile()
+    {
+        CreateMap<EmailTemplate, EmailTemplateDto>();
+        CreateMap<EmailTemplateDto, EmailTemplate>();
+    }
+}
