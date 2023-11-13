93da9d6dd8cee5fff1946f9222e417e6f4f9fd66
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-13 14:12:18 +0500

    Create listing rules dtos and profile

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Application/Availabilities/Dtos/ListingRulesRegistrationDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend_Project.Application.Availabilities.Dtos;
+
+public class ListingRulesRegistrationDto
+{
+    public Guid Id { get; set; }
+    public int Guests { get; set; }
+}
---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Infrastructure/Common/MapperProfiles/AvailabilitiesProfile/ListingRulesProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Backend_Project.Application.Availabilities.Dtos;
+using Backend_Project.Domain.Entities;
+
+namespace Backend_Project.Infrastructure.Common.MapperProfiles.AvailabilitiesProfile;
+
+public class ListingRulesProfile : Profile
+{
+    public ListingRulesProfile()
+    {
+        CreateMap<ListingRules, ListingRulesRegistrationDto>().ReverseMap();
+        CreateMap<ListingRules, ListingRulesDto>().ReverseMap();
+    }
+}
