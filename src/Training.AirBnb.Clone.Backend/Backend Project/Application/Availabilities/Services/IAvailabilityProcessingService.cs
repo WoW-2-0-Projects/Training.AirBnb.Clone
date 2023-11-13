ac4bac8fb03ca419908229adcf547b119c97400f
Ilxom <ilxomkarimjonov22@gmail.com>
2023-11-13 12:57:32 +0500

    Create availability processing service

---
+++ src/Training.AirBnb.Clone.Backend/Backend Project/Application/Availabilities/Services/IAvailabilityProcessingService.cs
@@ -0,0 +1,10 @@
+﻿using Backend_Project.Domain.Entities;
+
+namespace Backend_Project.Application.Availabilities.Services;
+
+public interface IAvailabilityProcessingService
+{
+    ValueTask<BlockedNight> CreateBlockedNightAsync(BlockedNight blockedNight, CancellationToken cancellationToken = default);
+
+    IList<BlockedNight> GetBlockedNightsByListingId(Guid listingId);
+}
