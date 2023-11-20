997d04335af444f257519f49fb558d2d7119859d
Azam Amonov <azamamonov555@gmail.com>
2023-11-20 17:43:34 +0500

    Change TContext DI

--- src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Repositories/EntityRepositoryBase.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Repositories/EntityRepositoryBase.cs
@@ -6,10 +6,10 @@ namespace AirBnB.Persistence.Repositories;
 
 public abstract class EntityRepositoryBase<TEntity, TContext> where TEntity : class, IEntity where TContext : DbContext
 {
-    protected TContext DbContext => (TContext) _dbContext;
-    private readonly DbContext _dbContext;
+    private TContext DbContext => _dbContext;
+    private readonly TContext _dbContext;
 
-    protected EntityRepositoryBase(DbContext dbContext)
+    protected EntityRepositoryBase(TContext dbContext)
     {
         _dbContext = dbContext;
     }
