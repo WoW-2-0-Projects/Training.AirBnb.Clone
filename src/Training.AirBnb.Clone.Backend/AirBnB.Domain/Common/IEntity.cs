041891bd7d305610b2af04d12946238f59461b9b
Azam Amonov <azamamonov555@gmail.com>
2023-11-22 09:50:04 +0500

    add docs

--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/IEntity.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/IEntity.cs
@@ -2,5 +2,11 @@ namespace AirBnB.Domain.Common;
 
 public interface IEntity
 {
+    /// <summary>
+    /// Gets or sets the unique identifier of the entity.
+    /// </summary>
+    /// <value>
+    /// The unique identifier of the entity.
+    /// </value>
     public Guid Id { get; set; }
 }
--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/IModificationAuditableEntity.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/IModificationAuditableEntity.cs
@@ -2,5 +2,11 @@ namespace AirBnB.Domain.Common;
 
 public interface IModificationAuditableEntity
 {
-    public Guid ModifiedBy { get; set; }
+    /// <summary>
+    /// Gets or sets the identifier of the user who last modified the entity.
+    /// </summary>
+    /// <value>
+    /// The identifier of the user who last modified the entity.
+    /// </value>
+    public Guid ModifiedBUserId { get; set; }
 }
