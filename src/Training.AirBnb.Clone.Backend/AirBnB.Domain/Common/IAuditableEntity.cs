8d89478ad7556ec3a29127c8f3d52f423f1a74ed
Azam Amonov <azamamonov555@gmail.com>
2023-11-22 09:39:07 +0500

    add docs

--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/IAuditableEntity.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/IAuditableEntity.cs
@@ -2,7 +2,19 @@ namespace AirBnB.Domain.Common;
 
 public interface IAuditableEntity : ISoftDeletedEntity
 {
+    /// <summary>
+    /// Gets or sets the date and time when the entity was created.
+    /// </summary>
+    /// <value>
+    /// The date and time when the entity was created.
+    /// </value>
     public DateTimeOffset CreatedTime { get; set; }
     
+    /// <summary>
+    /// Gets or sets the date and time when the entity was last updated.
+    /// </summary>
+    /// <value>
+    /// The date and time when the entity was last updated.
+    /// </value>
     public DateTimeOffset? UpdatedTime { get; set; }
 }
