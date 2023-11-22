e4c03e385d1da90fe4387b2055343a4cbdc631d3
Azam Amonov <azamamonov555@gmail.com>
2023-11-22 09:42:15 +0500

    add docs

--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/AuditableEntity.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/AuditableEntity.cs
@@ -1,8 +1,18 @@
 namespace AirBnB.Domain.Common;
 
+/// <summary>
+/// Represents an abstract class for entities that include audit information.
+/// </summary>
+/// 
 public abstract class AuditableEntity : SoftDeletedEntity, IAuditableEntity
 {
+    /// <summary>
+    /// Gets or sets the date and time when the entity was created.
+    /// </summary>
     public DateTimeOffset CreatedTime { get; set; }
     
+    /// <summary>
+    /// Gets or sets the date and time when the entity was last updated.
+    /// </summary>
     public DateTimeOffset? UpdatedTime { get; set; }
 }
