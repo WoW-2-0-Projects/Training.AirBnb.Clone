196659c5abddcb893f114e96fc8990d963eb8ef1
Azam Amonov <azamamonov555@gmail.com>
2023-11-22 09:31:46 +0500

    Changed prop name and add docs

--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/ICreationAuditableEntity.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/ICreationAuditableEntity.cs
@@ -1,6 +1,12 @@
 namespace AirBnB.Domain.Common;
 
+/// <summary>
+/// Gets or sets the identifier of the user who created the entity.
+/// </summary>
+/// <value>
+/// The identifier of the user who created the entity.
+/// </value>
 public interface ICreationAuditableEntity
 {
-    public Guid CreatedBy { get; set; }
+    public Guid CreatedByUserId { get; set; }
 }
