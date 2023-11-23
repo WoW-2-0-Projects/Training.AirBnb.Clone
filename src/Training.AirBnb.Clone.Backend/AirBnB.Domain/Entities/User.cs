03f2dbcd1ddc9caf2fac974cdb05a72f6bb38779
Bunyod <kbunyod3011@gmail.com>
2023-11-24 00:08:47 +0500

    Add User entity

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using AirBnB.Domain.Common;
+using AirBnB.Domain.Enums;
+
+namespace AirBnB.Domain.Entities;
+
+public sealed class User : SoftDeletedEntity
+{
+    public string FirstName { get; set; } = default!;
+
+    public string LastName { get; set; } = default!;
+
+    public string EmailAddress { get; set; } = default!;
+
+    public UserRole UserRole { get; set; } = default!;
+    
+    public bool IsActive { get; set; }
+
+    public Guid PhoneNumberId { get; set; } = default!;
+}
