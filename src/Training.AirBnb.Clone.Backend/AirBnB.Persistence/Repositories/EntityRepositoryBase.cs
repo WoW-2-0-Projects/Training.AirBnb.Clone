6e7e1fd2ec6d9475239305d081d18cd4f277c9de
Bunyod <kbunyod3011@gmail.com>
2023-11-24 12:09:20 +0500

    Fix some bugs

--- src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Repositories/EntityRepositoryBase.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Repositories/EntityRepositoryBase.cs
@@ -100,7 +100,7 @@ public abstract class EntityRepositoryBase<TEntity, TContext> where TEntity : cl
         return entity;
     }
 
-    protected async Task<TEntity> DeleteByIdAsync(
+    protected async ValueTask<TEntity?> DeleteByIdAsync(
                     Guid id,
                     bool saveChanges = true,
                     CancellationToken cancellationToken = default
