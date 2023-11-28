f785922c2e749f6e634752d388bd13ddb0e1d771
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-11-28 12:39:07 +0500

    add Dto models for Emailtemplate and SmsTemplate

--- src/Training.AirBnb.Clone.Backend/AirBnB.Api/Models/DTOs/EmailTemplateDTO.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Api/Models/DTOs/EmailTemplateDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace AirBnB.Api.Models.DTOs
+﻿using AirBnB.Domain.Enums;
+
+namespace AirBnB.Api.Models.DTOs;
+
+public class EmailTemplateDTO
 {
-    public class EmailTemplateDTO
-    {
-    }
+    public Guid Id { get; set; }
+
+    public string Subject { get; set; } = default!;
+
+    public string Content { get; set; } = default!;
+
+    public NotificationType Type { get; set; }
+
+    public NotificationTemplateType TemplateType { get; set; }
 }
--- src/Training.AirBnb.Clone.Backend/AirBnB.Api/Models/DTOs/SmsTemplateDTO.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Api/Models/DTOs/SmsTemplateDTO.cs
@@ -1,6 +1,14 @@
-﻿namespace AirBnB.Api.Models.DTOs
+﻿using AirBnB.Domain.Enums;
+
+namespace AirBnB.Api.Models.DTOs;
+
+public class SmsTemplateDTO
 {
-    public class SmsTemplateDTO
-    {
-    }
+    public Guid Id { get; set; }
+
+    public string Content { get; set; } = default!;
+
+    public NotificationType Type { get; set; }
+
+    public NotificationTemplateType TemplateType { get; set; }
 }
