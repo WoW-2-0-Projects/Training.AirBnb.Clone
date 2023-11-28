e0453957278fd6b465e8c29714d4e83b13561bb2
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-11-28 13:08:00 +0500

    add Controllers

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Api/Controllers/NotificationsController.cs
@@ -0,0 +1,25 @@
+﻿using AirBnB.Application.Common.Models.Querying;
+using AirBnB.Application.Common.Notifications.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirBnB.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationsController(IEmailTemplateService emailTemplateService,
+    ISmsTemplateService smsTemplateService) : ControllerBase
+{
+    [HttpGet("templates/sms")]
+    public async ValueTask<IActionResult> GetSmsTemplates([FromQuery] FilterPagination pagination)
+    {
+        var result = await smsTemplateService.GetByFilterAsync(pagination);
+        return Ok(result);
+    }
+
+    [HttpGet("templates/email")]
+    public async ValueTask<IActionResult> GetEmailTemplates([FromQuery] FilterPagination pagination)
+    {
+        var result = await emailTemplateService.GetByFilterAsync(pagination);
+        return Ok(result);
+    }
+}
