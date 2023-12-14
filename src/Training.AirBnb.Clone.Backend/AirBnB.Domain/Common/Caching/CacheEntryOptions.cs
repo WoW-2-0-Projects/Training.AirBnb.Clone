946be89962ad4b514141e81a362d875ec99e7e9f
Bunyod <kbunyod3011@gmail.com>
2023-12-15 01:17:29 +0500

    Add Cache models

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/Caching/CacheEntryOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace AirBnB.Domain.Common.Caching;
+
+public class CacheEntryOptions
+{
+    public TimeSpan? AbsoluteExpirationRelativeToNow { get; init; }
+    
+    public TimeSpan? SlidingExpiration { get; init; }
+
+    public CacheEntryOptions()
+    {
+    }
+
+    public CacheEntryOptions(TimeSpan absoluteExpirationRelativeToNow, TimeSpan slidingExpiration)
+    {
+        AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow;
+        SlidingExpiration = slidingExpiration;
+    }
+}
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/Caching/CacheModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AirBnB.Domain.Common.Caching;
+
+public abstract class CacheModel
+{
+    public abstract string CacheKey { get; }
+}
