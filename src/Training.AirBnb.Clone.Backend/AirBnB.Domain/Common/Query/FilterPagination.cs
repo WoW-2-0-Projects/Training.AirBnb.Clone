8bbcc52c738e2ce178402d9e08757a6c8cafcec2
Bunyod <kbunyod3011@gmail.com>
2023-12-15 01:33:43 +0500

    Add QuerySpecification model

--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/Query/FilterPagination.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/Query/FilterPagination.cs
@@ -2,5 +2,31 @@
 
 public class FilterPagination
 {
+    public uint PageSize { get; set; }
+    public uint PageToken { get; set; }
+
+    public FilterPagination()
+    {
+    }
     
+    public FilterPagination(uint pageSize, uint pageToken)
+    {
+        PageSize = pageSize;
+        PageToken = pageToken;
+    }
+
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        
+        hashCode.Add(PageSize);
+        hashCode.Add(PageToken);
+
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is FilterPagination filterPagination && filterPagination.GetHashCode() == GetHashCode();
+    }
 }
--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/Query/QuerySpecification.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/Query/QuerySpecification.cs
@@ -1,6 +1,36 @@
-﻿namespace AirBnB.Domain.Common.Query;
+﻿using System.Linq.Expressions;
+using AirBnB.Domain.Common.Caching;
+using AirBnB.Domain.Comparers;
 
-public class QuerySpecification
+namespace AirBnB.Domain.Common.Query;
+
+public class QuerySpecification<TEntity>(uint pageSize, uint pageToken) : CacheModel where TEntity : IEntity
 {
-    
+    public List<Expression<Func<TEntity, bool>>> FilteringOptions { get; } = new();
+
+    public List<(Expression<Func<TEntity, object>> KeySelector, bool IsAscending)> OrderingOptions { get; } = new();
+
+    public FilterPagination PaginationOptions { get; set; } = new(pageSize, pageToken);
+
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        foreach (var filter in FilteringOptions.Order(new PredicateExpressionComparer<TEntity>()))
+            hashCode.Add(filter.ToString());
+
+        foreach (var filter in OrderingOptions)
+            hashCode.Add(filter.ToString());
+        
+        hashCode.Add(PaginationOptions);
+
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is QuerySpecification<TEntity> querySpecification && querySpecification.GetHashCode() == GetHashCode();
+    }
+
+    public override string CacheKey => $"{typeof(TEntity).Name}_{GetHashCode()}";
 }
