3d1285cc1bddfb0e463be97fe2d349f73834fd78
Bunyod <kbunyod3011@gmail.com>
2023-12-15 19:36:38 +0500

    Add Cache Settings and RedisDistributedCacheBroker

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Caching/RedisDistributedCacheBroker.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using AirBnB.Domain.Common.Caching;
+using AirBnB.Infrastructure.Common.Settings;
+using AirBnB.Persistence.Caching;
+using Force.DeepCloner;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace AirBnB.Infrastructure.Common.Caching;
+
+public class RedisDistributedCacheBroker(IOptions<CacheSettings> cacheSettings, IDistributedCache distributedCache) : ICacheBroker
+{
+    private readonly DistributedCacheEntryOptions _entryOptions = new()
+    {
+        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(cacheSettings.Value.AbsoluteExpirationInSeconds),
+        SlidingExpiration = TimeSpan.FromSeconds(cacheSettings.Value.SlidingExpirationInSeconds)
+    };
+
+
+    public async ValueTask<T?> GetAsync<T>(string key)
+    {
+        var value = await distributedCache.GetAsync(key);
+
+        return value is not null ? JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(value)) : default;
+    }
+
+    public ValueTask<bool> TryGetAsync<T>(string key, out T? value)
+    {
+        var foundEntry = distributedCache.GetString(key);
+
+        if (foundEntry is not null)
+        {
+            value = JsonConvert.DeserializeObject<T>(foundEntry);
+            return ValueTask.FromResult(true);
+        }
+
+        value = default;
+        return ValueTask.FromResult(false);
+    }
+
+    public async ValueTask<T?> GetOrSetAsync<T>(string key, Func<Task<T>> valueFactory, CacheEntryOptions? cacheEntryOptions = default)
+    {
+        var cachedValue = await distributedCache.GetStringAsync(key);
+        if (cachedValue is not null) return JsonConvert.DeserializeObject<T>(cachedValue);
+
+        var value = await valueFactory();
+
+        await SetAsync(key, await valueFactory(), cacheEntryOptions);
+
+        return value;
+    }
+
+    public async ValueTask SetAsync<T>(string key, T value, CacheEntryOptions? entryOptions = default)
+    {
+        await distributedCache.SetStringAsync(key, JsonConvert.SerializeObject(value), GetCacheEntryOptions(entryOptions));
+    }
+
+    public ValueTask DeleteAsync(string key)
+    {
+        distributedCache.Remove(key);
+        return ValueTask.CompletedTask;
+    }
+
+
+    private DistributedCacheEntryOptions GetCacheEntryOptions(CacheEntryOptions? entryOptions)
+    {
+        if (entryOptions == default || (!entryOptions.AbsoluteExpirationRelativeToNow.HasValue && !entryOptions.SlidingExpiration.HasValue))
+            return _entryOptions;
+
+        var currentEntryOptions = _entryOptions.DeepClone();
+
+        currentEntryOptions.AbsoluteExpirationRelativeToNow = entryOptions.AbsoluteExpirationRelativeToNow;
+        currentEntryOptions.SlidingExpiration = entryOptions.SlidingExpiration;
+
+        return currentEntryOptions;
+    }
+}
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Settings/CacheSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace AirBnB.Infrastructure.Common.Settings;
+
+public class CacheSettings
+{
+    public int AbsoluteExpirationInSeconds { get; set; }
+
+    public int SlidingExpirationInSeconds { get; set; }
+}
