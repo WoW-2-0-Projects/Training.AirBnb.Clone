8fa51fa5a6fcc41af84a44d313d009cedbc02402
Bunyod <kbunyod3011@gmail.com>
2023-12-17 20:09:44 +0500

    Simplify the code

--- src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Repositories/UserRepository.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Repositories/UserRepository.cs
@@ -9,37 +9,41 @@ using AirBnB.Persistence.Repositories.Interfaces;
 namespace AirBnB.Persistence.Repositories;
 
 public class UserRepository(IdentityDbContext dbContext, ICacheBroker cacheBroker)
-    : EntityRepositoryBase<User, IdentityDbContext>(dbContext, cacheBroker), IUserRepository
+    : EntityRepositoryBase<User, IdentityDbContext>(
+        dbContext, 
+        cacheBroker,
+        new CacheEntryOptions()
+        ), IUserRepository
 {
-    public IQueryable<User> Get(Expression<Func<User, bool>>? predicate, bool asNoTracking = false)
+    public new IQueryable<User> Get(Expression<Func<User, bool>>? predicate, bool asNoTracking = false)
         => base.Get(predicate, asNoTracking);
 
-    public ValueTask<IList<User>> GetAsync(QuerySpecification<User> querySpecification, bool asNoTracking = false,
+    public new ValueTask<IList<User>> GetAsync(QuerySpecification<User> querySpecification, bool asNoTracking = false,
         CancellationToken cancellationToken = default)
         => base.GetAsync(querySpecification, asNoTracking, cancellationToken);
 
 
-    public ValueTask<User?> GetByIdAsync(Guid userId, bool asNoTracking = false, CancellationToken cancellationToken = default)
+    public new ValueTask<User?> GetByIdAsync(Guid userId, bool asNoTracking = false, CancellationToken cancellationToken = default)
         => base.GetByIdAsync(userId, asNoTracking, cancellationToken);
 
 
-    public ValueTask<IList<User>> GetByIdsAsync(IEnumerable<Guid> ids, bool asNoTracking = false, CancellationToken cancellationToken = default)
+    public new ValueTask<IList<User>> GetByIdsAsync(IEnumerable<Guid> ids, bool asNoTracking = false, CancellationToken cancellationToken = default)
         => base.GetByIdsAsync(ids, asNoTracking, cancellationToken);
 
 
-    public ValueTask<User> CreateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
+    public new ValueTask<User> CreateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
         => base.CreateAsync(user, saveChanges, cancellationToken);
 
 
-    public ValueTask<User> UpdateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
+    public new ValueTask<User> UpdateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
         => base.UpdateAsync(user, saveChanges, cancellationToken);
 
 
-    public ValueTask<User?> DeleteAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
+    public new ValueTask<User?> DeleteAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
         => base.DeleteAsync(user, saveChanges, cancellationToken);
 
 
-    public ValueTask<User?> DeleteByIdAsync(Guid userId, bool saveChanges = true, CancellationToken cancellationToken = default)
+    public new ValueTask<User?> DeleteByIdAsync(Guid userId, bool saveChanges = true, CancellationToken cancellationToken = default)
         => base.DeleteByIdAsync(userId, saveChanges, cancellationToken);
 }
 
