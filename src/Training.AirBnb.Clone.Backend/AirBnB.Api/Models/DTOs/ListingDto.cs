e80babde4bd7f895f076a94c1f7b4dd7b0f5d568
Botirjon <thedavlativich@gmail.com>
2023-12-19 19:46:42 +0500

    add listing data transfer object (dto)

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Api/Models/DTOs/ListingDto.cs
@@ -0,0 +1,22 @@
+﻿namespace AirBnB.Api.Models.DTOs;
+
+/// <summary>
+/// Data Transfer Object (DTO) representing a listing.
+/// </summary>
+public class ListingDto
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the listing
+    /// </summary>
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the title of the listing.
+    /// </summary>
+    public string Title { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the price of the listing.
+    /// </summary>
+    public decimal? Price { get; set; }
+}
