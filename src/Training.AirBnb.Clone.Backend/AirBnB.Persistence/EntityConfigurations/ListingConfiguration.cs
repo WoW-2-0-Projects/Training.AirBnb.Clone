27da66701b346fd7a5a9e33535d8496deba89cb7
Botirjon <thedavlativich@gmail.com>
2023-12-19 19:38:50 +0500

    add listing configuration

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/EntityConfigurations/ListingConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using AirBnB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AirBnB.Persistence.EntityConfigurations;
+
+/// <summary>
+/// Entity type configuration for the Listing class.
+/// </summary>
+public class ListingConfiguration : IEntityTypeConfiguration<Listing>
+{
+    public void Configure(EntityTypeBuilder<Listing> builder)
+    {
+        builder.Property(listing => listing.Title).IsRequired().HasMaxLength(128);
+    }
+}
