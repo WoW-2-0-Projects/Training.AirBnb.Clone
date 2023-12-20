5d54487a0080fc474b9b09e6d7019ca37c4f4082
Botirjon <thedavlativich@gmail.com>
2023-12-20 18:31:29 +0500

    add router to Initialize AirBnb Client project

---
+++ src/Training.AirBnb.Clone.Frontend/src/infrastructure/router/index.ts
@@ -0,0 +1,9 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const index = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+    ]
+})
+
+export default index
--- src/Training.AirBnb.Clone.Frontend/src/main.ts
+++ src/Training.AirBnb.Clone.Frontend/src/main.ts
@@ -4,11 +4,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import index from './infrastructure/router/index'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(index)
 
 app.mount('#app')
