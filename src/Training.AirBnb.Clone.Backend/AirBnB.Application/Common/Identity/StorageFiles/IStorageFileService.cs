baf7569615fb6df1ccfe4ee57c1a505b7ad4bbd5
Bunyod <kbunyod3011@gmail.com>
2023-12-23 01:46:08 +0500

    Add  StorageFileService

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Identity/StorageFiles/IStorageFileService.cs
@@ -0,0 +1,11 @@
+﻿using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities;
+
+namespace AirBnB.Application.Common.Identity.StorageFiles;
+
+public interface IStorageFileService
+{
+    ValueTask<IList<StorageFile>> GetAsync(QuerySpecification<StorageFile> querySpecification, CancellationToken cancellationToken = default);
+
+    ValueTask<IList<StorageFile>> GetAsync(QuerySpecification querySpecification, CancellationToken cancellationToken = default);
+}
