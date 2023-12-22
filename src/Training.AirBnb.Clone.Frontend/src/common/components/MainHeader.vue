51a93acdfc569d5f12c494fc2a535ead6b348e60
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-12-23 04:31:44 +0500

    Site header is doing.

---
+++ src/Training.AirBnb.Clone.Frontend/src/common/components/MainHeader.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div
+      class="fixed h-20 top-0 z-10 content-padding w-full bg-defaultBackground flex justify-between items-center border-b border-borderSecondary text-textPrimary">
+
+    <!-- Logo -->
+    <main-logo/>
+
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+import MainLogo from "@/common/components/icons/MainLogo.vue";
+
+</script>
+
