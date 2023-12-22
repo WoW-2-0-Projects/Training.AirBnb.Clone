baf1f86b0005c73bf7617dcb6d5fc30f28aa849e
Botirjon <thedavlativich@gmail.com>
2023-12-22 13:57:41 +0500

    Add User Info Verification code entity

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Entities/UserInfoVerificationCode.cs
@@ -0,0 +1,22 @@
+﻿using AirBnB.Domain.Enums;
+
+namespace AirBnB.Domain.Entities;
+
+/// <summary>
+/// Represents a verification code specifically for user information.
+/// </summary>
+public class UserInfoVerificationCode : VerificationCode
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserInfoVerificationCode"/> class.
+    /// </summary>
+    public UserInfoVerificationCode()
+    {
+        Type = VerificationType.UserInfoVerificationCode;
+    }
+
+    /// <summary>
+    /// Gets or sets the unique identifier of the user associated with the verification code.
+    /// </summary>
+    public Guid UserId { get; set; }
+}
