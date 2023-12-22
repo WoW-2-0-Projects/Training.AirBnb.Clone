972438a00a08bb75b94ea6e9e17ee4f9b191b335
Bunyod <kbunyod3011@gmail.com>
2023-12-23 01:29:59 +0500

    Add StorageFileRepository

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Repositories/Interfaces/IStorageFileRepository.cs
@@ -0,0 +1,11 @@
+﻿using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities;
+
+namespace AirBnB.Persistence.Repositories.Interfaces;
+
+public interface IStorageFileRepository
+{
+    ValueTask<IList<StorageFile>> GetAsync(QuerySpecification<StorageFile> querySpecification, CancellationToken cancellationToken = default);
+
+    ValueTask<IList<StorageFile>> GetAsync(QuerySpecification querySpecification, CancellationToken cancellationToken = default);
+}
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Repositories/StorageFileRepository.cs
@@ -0,0 +1,21 @@
+﻿using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities;
+using AirBnB.Persistence.Caching.Brokers;
+using AirBnB.Persistence.Caching.Models;
+using AirBnB.Persistence.DataContexts;
+using AirBnB.Persistence.Repositories.Interfaces;
+
+namespace AirBnB.Persistence.Repositories;
+
+public class StorageFileRepository(IdentityDbContext dbContext, ICacheBroker cacheBroker) : EntityRepositoryBase<StorageFile, IdentityDbContext>(
+    dbContext,
+    cacheBroker, 
+    new CacheEntryOptions()), IStorageFileRepository
+{
+    public new ValueTask<IList<StorageFile>> GetAsync(QuerySpecification<StorageFile> querySpecification,
+        CancellationToken cancellationToken = default)
+        => base.GetAsync(querySpecification, cancellationToken);
+
+    public new ValueTask<IList<StorageFile>> GetAsync(QuerySpecification querySpecification, CancellationToken cancellationToken = default)
+        => base.GetAsync(querySpecification, cancellationToken);
+}
