3ef06982e214cd48a718dea6879669b47ac9e69c
Botirjon <thedavlativich@gmail.com>
2023-12-23 20:34:24 +0500

    Update entities and enum

--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Entities/Address.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Entities/Address.cs
@@ -1,12 +1,27 @@
 ﻿namespace AirBnB.Domain.Entities;
 
+/// <summary>
+/// Represents a geographical address with information.
+/// </summary>
 public class Address
 {
+    /// <summary>
+    /// Gets or sets the name of the city in the address.
+    /// </summary>
     public string? City { get; set; }
-    
+
+    /// <summary>
+    /// Gets or sets the unique identifier of the city in the address.
+    /// </summary>
     public Guid? CityId { get; set; }
-    
+
+    /// <summary>
+    /// Gets or sets the latitude coordinate of the address.
+    /// </summary>
     public double Latitude { get; set; }
-    
+
+    /// <summary>
+    /// Gets or sets the longitude coordinate of the address.
+    /// </summary>
     public double Longitude { get; set; }
 }
--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Entities/Money.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Entities/Money.cs
@@ -2,9 +2,18 @@
 
 namespace AirBnB.Domain.Entities;
 
+/// <summary>
+/// Represents a monetary value.
+/// </summary>
 public class Money
 {
+    /// <summary>
+    /// Gets or sets the numerical amount of money.
+    /// </summary>
     public decimal Amount { get; set; }
-    
+
+    /// <summary>
+    /// Gets or sets the currency associated with the monetary amount.
+    /// </summary>
     public Currency Currency { get; set; }
 }
