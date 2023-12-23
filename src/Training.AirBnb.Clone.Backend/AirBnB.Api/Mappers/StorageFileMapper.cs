e4d8f2bbfb53531f44ae9ccebea16f617dcca0eb
Bunyod <kbunyod3011@gmail.com>
2023-12-23 17:45:51 +0500

    Add Mappers

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Api/Mappers/StorageFileMapper.cs
@@ -0,0 +1,13 @@
+﻿using AirBnB.Api.Models.DTOs;
+using AirBnB.Domain.Entities;
+using AutoMapper;
+
+namespace AirBnB.Api.Mappers;
+
+public class StorageFileMapper : Profile
+{
+    public StorageFileMapper()
+    {
+        CreateMap<StorageFile, StorageFileDto>().ReverseMap();
+    }
+}
