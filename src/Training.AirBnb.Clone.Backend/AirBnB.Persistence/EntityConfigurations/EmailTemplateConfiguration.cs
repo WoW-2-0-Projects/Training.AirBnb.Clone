82a83529993697762f94f2e181165ea44199b754
Botirjon <thedavlativich@gmail.com>
2023-12-23 05:18:05 +0500

    Add DbContext and Configuration

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/EntityConfigurations/EmailTemplateConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using AirBnB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AirBnB.Persistence.EntityConfigurations;
+
+public class EmailTemplateConfiguration : IEntityTypeConfiguration<EmailTemplate>
+{
+    public void Configure(EntityTypeBuilder<EmailTemplate> builder)
+    {
+        builder.Property(emailTemplate => emailTemplate.Subject).IsRequired().HasMaxLength(256);
+    }
+}
