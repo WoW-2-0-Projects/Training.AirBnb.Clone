74b8eb01564473f253465538b401adb9932a1662
Botirjon <thedavlativich@gmail.com>
2023-12-23 05:17:29 +0500

    Add EmailTemplateRepository and SmsTemplateRepository

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Repositories/EmailTemplateRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities;
+using AirBnB.Persistence.Caching.Brokers;
+using AirBnB.Persistence.Caching.Models;
+using AirBnB.Persistence.DataContexts;
+using AirBnB.Persistence.Repositories.Interfaces;
+
+namespace AirBnB.Persistence.Repositories;
+
+public class EmailTemplateRepository(NotificationDbContext dbContext, ICacheBroker cacheBroker):
+    EntityRepositoryBase<EmailTemplate, NotificationDbContext>(dbContext, cacheBroker, new CacheEntryOptions()), IEmailTemplateRepository
+{
+    public new IQueryable<EmailTemplate> Get(
+        Expression<Func<EmailTemplate, bool>>? predicate = default,
+        bool asNoTracking = false) => base.Get(predicate, asNoTracking);
+
+    public new ValueTask<IList<EmailTemplate>> GetAsync(
+        QuerySpecification<EmailTemplate> querySpecification,
+        CancellationToken cancellationToken = default) =>
+        base.GetAsync(querySpecification, cancellationToken);
+
+    public new ValueTask<IList<EmailTemplate>> GetAsync(
+        QuerySpecification querySpecification,
+        CancellationToken cancellationToken = default) =>
+        base.GetAsync(querySpecification, cancellationToken);
+    
+
+    public new async ValueTask<EmailTemplate> CreateAsync(EmailTemplate emailTemplate, bool saveChanges = true,
+        CancellationToken cancellationToken = default) =>
+        await base.CreateAsync(emailTemplate, saveChanges, cancellationToken);
+}
