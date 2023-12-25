40e459d53fcbaacb180211097d8c17466f9ad453
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2023-12-25 19:08:09 +0500

    Created TemplatePlaceholder model

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Notifications/Models/TemplatePlaceholder.cs
@@ -0,0 +1,12 @@
+﻿namespace AirBnB.Application.Common.Notifications.Models;
+
+public class TemplatePlaceholder
+{
+    public string Placeholder { get; set; } = default!;
+
+    public string PlaceholderValue { get; set; } = default!;
+
+    public string? Value { get; set; } = default!;
+
+    public bool IsValid { get; set; }
+}
