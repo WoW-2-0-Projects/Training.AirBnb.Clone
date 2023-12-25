92af1630a88b45403b8cd5f518566b2cad2a0f81
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2023-12-25 19:07:36 +0500

    Created Validators for email and sms message models

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Validators/SmsMessageValidator.cs
@@ -0,0 +1,27 @@
+﻿using AirBnB.Application.Common.Notifications.Models;
+using AirBnB.Domain.Enums;
+using FluentValidation;
+
+namespace AirBnB.Infrastructure.Common.Validators;
+
+public class SmsMessageValidator : AbstractValidator<SmsMessage>
+{
+    public SmsMessageValidator()
+    {
+        RuleSet(NotificationEvent.OnRendering.ToString(),
+            () =>
+            {
+                RuleFor(message => message.Template).NotNull();
+                RuleFor(message => message.Variables).NotNull();
+                RuleFor(message => message.Template.Content).NotNull().NotEmpty();
+            });
+
+        RuleSet(NotificationEvent.OnSending.ToString(),
+            () =>
+            {
+                RuleFor(message => message.SendEmailAddress).NotNull().NotEmpty();
+                RuleFor(message => message.ReceiverEmailAddress).NotNull().NotEmpty();
+                RuleFor(message => message.Message).NotNull().NotEmpty();
+            });
+    }
+}
