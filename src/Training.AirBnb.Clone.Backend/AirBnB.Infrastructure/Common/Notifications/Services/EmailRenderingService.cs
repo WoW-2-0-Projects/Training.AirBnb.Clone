ececd54d4a64a44f3c3df482debec5e5ae59a6b4
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2023-12-25 19:08:41 +0500

    Implement Sms and Email rendering services

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Notifications/Services/EmailRenderingService.cs
@@ -0,0 +1,83 @@
+﻿using AirBnB.Application.Common.Notifications.Models;
+using AirBnB.Application.Common.Notifications.Services;
+using AirBnB.Application.Common.Settings;
+using AirBnB.Domain.Enums;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AirBnB.Infrastructure.Common.Notifications.Services;
+
+public class EmailRenderingService(
+    IValidator<EmailMessage> emailMessageValidator,
+    IOptions<TemplateRenderingSettings> templateRenderingSettingsOptions
+    ) : IEmailRenderingService
+{
+    private readonly TemplateRenderingSettings _templateRenderingSettings = templateRenderingSettingsOptions.Value;
+
+    public ValueTask<string> RenderAsync(
+    EmailMessage emailMessage, 
+        CancellationToken cancellationToken = default)
+    {
+        var validationResult = emailMessageValidator.Validate(emailMessage,
+            options => options.IncludeRuleSets(NotificationEvent.OnRendering.ToString()));
+
+        if (!validationResult.IsValid) throw new ValidationException(validationResult.Errors);
+
+        var placeholderRegex = new Regex(_templateRenderingSettings.PlaceholderRegexPattern,
+            RegexOptions.Compiled,
+            TimeSpan.FromSeconds(_templateRenderingSettings.RegexMatchTimeoutInSeconds));
+
+        var placeholderValueRegex = new Regex(_templateRenderingSettings.PlaceholderValueRegexPattern,
+            RegexOptions.Compiled,
+            TimeSpan.FromSeconds(_templateRenderingSettings.RegexMatchTimeoutInSeconds));
+
+        var matches = placeholderRegex.Matches(emailMessage.Template.Content);
+
+        if (matches.Any() && !emailMessage.Variables.Any())
+            throw new InvalidOperationException("Variables are required for this template.");
+
+        var templatePlaceholders = matches.Select(match =>
+        {
+            var placeholder = match.Value;
+            var placeholderValue = placeholderValueRegex.Match(placeholder).Groups[1].Value;
+            var valid = emailMessage.Variables.TryGetValue(placeholderValue, out var value);
+
+            return new TemplatePlaceholder
+            {
+                Placeholder = placeholder,
+                PlaceholderValue = placeholderValue,
+                Value = value,
+                IsValid = valid
+            };
+        })
+            .ToList();
+
+        ValidatePlaceholders(templatePlaceholders);
+
+        var messageBuilder = new StringBuilder(emailMessage.Template.Content);
+        templatePlaceholders.ForEach(placeholder => messageBuilder.Replace(placeholder.Placeholder, placeholder.Value));
+
+        var message = messageBuilder.ToString();
+        emailMessage.Body = message;
+        emailMessage.Subject = emailMessage.Template.Subject;
+
+        return ValueTask.FromResult(message);
+    }
+
+    private void ValidatePlaceholders(IEnumerable<TemplatePlaceholder> templatePlaceholders)
+    {
+        var missingPlaceholders = templatePlaceholders.Where(placeholder => !placeholder.IsValid)
+            .Select(placeholder => placeholder.PlaceholderValue)
+            .ToList();
+
+        if (!missingPlaceholders.Any()) return;
+
+        var errorMessage = new StringBuilder();
+        missingPlaceholders.ForEach(placeholderValue => errorMessage.Append(placeholderValue).Append(','));
+
+        throw new InvalidOperationException(
+            $"Variable for given placeholders is not found - {string.Join(',', missingPlaceholders)}");
+    }
+}
