d804ad4b7a27f8c7e47555c2098adabf43780b08
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2023-12-25 19:05:08 +0500

    created notification, email and sms message models

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Notifications/Models/EmailMessage.cs
@@ -0,0 +1,34 @@
+﻿using AirBnB.Domain.Entities;
+
+namespace AirBnB.Application.Common.Notifications.Models;
+
+/// <summary>
+/// Represents an email message for notifications
+/// </summary>
+public class EmailMessage : NotificationMessage
+{
+    /// <summary>
+    /// Gets or sets email address of sender user
+    /// </summary>
+    public string SendEmailAddress { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets email address of receiver user
+    /// </summary>
+    public string ReceiverEmailAddress { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets email template of the email message
+    /// </summary>
+    public EmailTemplate Template { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets subject of the email message
+    /// </summary>
+    public string Subject { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets body of the email message
+    /// </summary>
+    public string Body { get; set; } = default!;
+}
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Notifications/Models/SmsMessage.cs
@@ -0,0 +1,29 @@
+﻿using AirBnB.Domain.Entities;
+
+namespace AirBnB.Application.Common.Notifications.Models;
+
+/// <summary>
+/// Represents an sms message for notifications
+/// </summary>
+public class SmsMessage : NotificationMessage
+{
+    /// <summary>
+    /// Gets or sets sms address of sender user
+    /// </summary>
+    public string SendEmailAddress { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets sms address of receiver user
+    /// </summary>
+    public string ReceiverEmailAddress { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets sms template of the sms message
+    /// </summary>
+    public SmsTemplate Template { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets message of the sms message
+    /// </summary>
+    public string Message { get; set; } = default!;
+}
