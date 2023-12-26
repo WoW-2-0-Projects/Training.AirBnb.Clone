05ebf362cbdfbd4b36d219be49235d4070a27a38
Jasurbek1223 <jasurabdulxaev@gmail.com>
2023-12-26 15:27:30 +0500

    Created AccessToken model

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Entities/AccessToken.cs
@@ -0,0 +1,27 @@
+﻿using AirBnB.Domain.Common;
+
+namespace AirBnB.Domain.Entities;
+
+public class AccessToken : Entity
+{
+    public AccessToken()
+    {
+    }
+
+    public AccessToken(Guid id, Guid userId, string token, DateTimeOffset expiryTime, bool isRevoked)
+    {
+        Id = id;
+        UserId = userId;
+        Token = token;
+        Expirytime = expiryTime;
+        IsRevoked = isRevoked;
+    }
+    
+    public Guid UserId { get; set; }
+
+    public string Token { get; set; } = default!;
+    
+    public DateTimeOffset Expirytime { get; set; }
+    
+    public bool IsRevoked { get; set; }
+}
