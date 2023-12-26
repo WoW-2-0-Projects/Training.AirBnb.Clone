a2c005f4f451a2adb229bd4693aea81f5cdd51d1
Jasurbek1223 <jasurabdulxaev@gmail.com>
2023-12-26 22:22:06 +0500

    Added AccessTokenValidator service

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Validators/AccessTokenValidator.cs
@@ -0,0 +1,61 @@
+﻿using AirBnB.Domain.Entities;
+using AirBnB.Domain.Enums;
+using AirBnB.Infrastructure.Common.Settings;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace AirBnB.Infrastructure.Common.Validators;
+
+public class AccessTokenValidator : AbstractValidator<AccessToken>
+{
+
+    public AccessTokenValidator(IOptions<JwtSettings> jwtSettings)
+    {
+        var jwtSettingsValue = jwtSettings.Value;
+
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(accessToken => accessToken.IsRevoked).NotEqual(true);
+
+                RuleFor(accessToken => accessToken.UserId).NotEqual(Guid.Empty);
+            }
+        );
+
+        RuleSet(
+            EntityEvent.OnUpdate.ToString(),
+            () =>
+            {
+                RuleFor(accesstoken => accesstoken.Token).NotEmpty();
+
+                RuleFor(accesstoken => accesstoken.ExpiryTime)
+                    .GreaterThan(DateTimeOffset.UtcNow)
+                    .Custom(
+                        (accesstoken, context) =>
+                        {
+                            if (accesstoken > DateTimeOffset.UtcNow.AddMinutes(jwtSettingsValue.ExpirationTimeInMinute))
+                                context.AddFailure(
+                                    nameof(AccessToken.ExpiryTime),
+                                    $"{nameof(AccessToken.ExpiryTime)} cannot be greater than the expiration time of the JWT token"
+                                );
+                        }
+                    );
+
+                RuleFor(accessToken => accessToken)
+                    .Custom(
+                        (accessToken, context) =>
+                        {
+                            if (context.RootContextData.TryGetValue(nameof(AccessToken), out var userInfoObj) &&
+                                userInfoObj is AccessToken foundAccessToken)
+                                if (accessToken.UserId != foundAccessToken.UserId)
+                                    context.AddFailure(nameof(AccessToken.UserId),
+                                        $"{nameof(AccessToken.UserId)} cannot be changed.");
+                        }
+                    );
+
+                RuleFor(accessToken => accessToken.IsRevoked).NotEqual(true);
+            }
+        );
+    }
+}
