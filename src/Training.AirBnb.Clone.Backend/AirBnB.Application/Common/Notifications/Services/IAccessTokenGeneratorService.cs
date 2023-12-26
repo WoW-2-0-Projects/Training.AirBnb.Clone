26519b66d8a8efd6faad05abe346a69e7363e725
Jasurbek1223 <jasurabdulxaev@gmail.com>
2023-12-26 20:04:36 +0500

    Created IAccessTokenGeneratorService and IAccessTokenService interfaces

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Notifications/Services/IAccessTokenGeneratorService.cs
@@ -0,0 +1,10 @@
+﻿using AirBnB.Domain.Entities;
+
+namespace AirBnB.Application.Common.Notifications.Services;
+
+public interface IAccessTokenGeneratorService
+{
+    AccessToken GetToken(User user);
+
+    Guid GettokenId(string accessToken);
+}
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Notifications/Services/IAccessTokenService.cs
@@ -0,0 +1,13 @@
+﻿using AirBnB.Domain.Entities;
+
+namespace AirBnB.Application.Common.Notifications.Services;
+
+public interface IAccessTokenService
+{
+    ValueTask<AccessToken> CreateAsync(AccessToken accessToken, bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<AccessToken?> GetByIdAsync(Guid accessTokenId, CancellationToken cancellationToken = default);
+
+    ValueTask RevokeAsync(Guid accessTokenId, CancellationToken cancellationToken = default);
+}
