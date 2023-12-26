262d71208b514b638396ba79fc20120202d5bd03
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-26 08:01:31 +0500

    Refactor identity db context

--- src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/DataContexts/IdentityDbContext.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/DataContexts/IdentityDbContext.cs
@@ -1,22 +1,19 @@
 ﻿using AirBnB.Domain.Entities.Identity;
 using AirBnB.Domain.Entities.StorageFiles;
+using AirBnB.Persistence.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace AirBnB.Persistence.DataContexts;
 
-public class IdentityDbContext : DbContext
+public class IdentityDbContext(DbContextOptions<IdentityDbContext> dbContextOptions) : DbContext(dbContextOptions)
 {
     public DbSet<User> Users => Set<User>();
 
     public DbSet<StorageFile> StorageFiles => Set<StorageFile>();
-    
-    public IdentityDbContext(DbContextOptions<IdentityDbContext> dbContextOptions) : base(dbContextOptions)
-    {
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("identity");
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IdentityDbContext).Assembly);
+        modelBuilder.ApplyEntityConfigurations<IdentityDbContext>();
     }
 }
