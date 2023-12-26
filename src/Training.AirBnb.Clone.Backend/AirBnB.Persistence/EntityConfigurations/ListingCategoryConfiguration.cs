8e16fdc25deb5db6973743f8129317276b99c58d
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-26 07:56:09 +0500

    Add Listing Category entity configuration

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/EntityConfigurations/ListingCategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AirBnB.Domain.Entities.Listings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AirBnB.Persistence.EntityConfigurations;
+
+public class ListingCategoryConfiguration : IEntityTypeConfiguration<ListingCategory>
+{
+    public void Configure(EntityTypeBuilder<ListingCategory> builder)
+    {
+        builder.Property(category => category.Name).IsRequired().HasMaxLength(64);
+        builder.Property(category => category.StorageFileId).IsRequired();
+
+        builder.HasIndex(category => category.Name).IsUnique();
+
+        builder.HasOne(category => category.ImageStorageFile).WithOne().HasForeignKey(typeof(ListingCategory), "StorageFileId");
+    }
+}
