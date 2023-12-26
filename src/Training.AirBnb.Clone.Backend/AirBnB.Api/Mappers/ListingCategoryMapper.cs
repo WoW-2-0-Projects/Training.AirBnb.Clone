bf5abba613143fceed1898b43444aa6b43399712
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-26 07:47:33 +0500

    Add Listing Category mapper

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Api/Mappers/ListingCategoryMapper.cs
@@ -0,0 +1,21 @@
+﻿using AirBnB.Api.Models.DTOs.Listings;
+using AirBnB.Domain.Entities.Listings;
+using AirBnB.Domain.Entities.StorageFiles;
+using AirBnB.Infrastructure.StorageFiles.Mappers;
+using AutoMapper;
+
+namespace AirBnB.Api.Mappers;
+
+/// <summary>
+/// AutoMapper profile for mapping between the User and UserDto classes.
+/// </summary>
+public class ListingCategoryMapper : Profile
+{
+    /// <summary>
+    /// Initializes a new instance of the ListingCategoryMapper
+    /// </summary>
+    public ListingCategoryMapper()
+    {
+        CreateMap<ListingCategory, ListingCategoryDto>().ForMember(dest => dest.ImageUrl, opt => opt.ConvertUsing<StorageFileToUrlConverter, StorageFile>(src => src.ImageStorageFile));
+    }
+}
