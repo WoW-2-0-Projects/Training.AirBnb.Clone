a9379aa54e35df0ee574aef1507a1345dbcb36e3
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-26 08:10:26 +0500

    Add Listings controller

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Api/Controllers/ListingsController.cs
@@ -0,0 +1,23 @@
+﻿using AirBnB.Api.Models.DTOs.Listings;
+using AirBnB.Application.Listings.Models;
+using AirBnB.Application.Listings.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirBnB.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ListingsController(IMapper mapper) : ControllerBase
+{
+    [HttpGet("categories")]
+    public async ValueTask<IActionResult> GetListingCategories(
+        [FromServices] IListingCategoryService listingCategoryService, 
+        CancellationToken cancellationToken = default)
+    {
+        var result = await listingCategoryService
+            .GetAsync(new ListingCategoryFilter().ToQuerySpecification(), cancellationToken);
+
+        return result.Any() ? Ok(result.Select(mapper.Map<List<ListingCategoryDto>>)) : NoContent();
+    }
+}
