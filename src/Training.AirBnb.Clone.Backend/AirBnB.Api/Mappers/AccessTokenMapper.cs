940eb808b4a967bfb745d100b21756b1c71eaad8
Jasurbek1223 <jasurabdulxaev@gmail.com>
2023-12-26 23:03:23 +0500

    Add comments

--- src/Training.AirBnb.Clone.Backend/AirBnB.Api/Mappers/AccessTokenMapper.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Api/Mappers/AccessTokenMapper.cs
@@ -4,10 +4,17 @@ using AutoMapper;
 
 namespace AirBnB.Api.Mappers;
 
+/// <summary>
+/// AutoMapper profile for mapping between AccessToken and AccessTokenDto.
+/// </summary>
 public class AccessTokenMapper : Profile
 {
+    /// <summary>
+    /// Initializes a new instance of the AccessTokenMapper class and configures the mapping between AccessToken and AccessTokenDto.
+    /// </summary>
     public AccessTokenMapper()
     {
+        // Configure mapping from AccessToken to AccessTokenDto and enable reverse mapping.
         CreateMap<AccessToken, AccessTokenDto>().ReverseMap();
     }
 }
--- src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Serializers/IJsonSerializationSettingsProvider.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Serializers/IJsonSerializationSettingsProvider.cs
@@ -2,7 +2,15 @@
 
 namespace AirBnB.Application.Common.Serializers;
 
+/// <summary>
+/// Interface for providing JSON serialization settings.
+/// </summary>
 public interface IJsonSerializationSettingsProvider
 {
+    /// <summary>
+    /// Gets JSON serialization settings.
+    /// </summary>
+    /// <param name="clone">Indicates whether to return a clone of the settings (default is false).</param>
+    /// <returns>JsonSerializerSettings for customizing JSON serialization.</returns>
     JsonSerializerSettings Get(bool clone = false);
 }
