ebe6849d402b33ef7e876ba449999109014dcb9d
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-26 08:00:57 +0500

    Refactor notification db context

--- src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/DataContexts/NotificationDbContext.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/DataContexts/NotificationDbContext.cs
@@ -1,22 +1,18 @@
 using AirBnB.Domain.Entities.Notification;
+using AirBnB.Persistence.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace AirBnB.Persistence.DataContexts;
 
-public class NotificationDbContext : DbContext
+public class NotificationDbContext(DbContextOptions<NotificationDbContext> options) : DbContext(options)
 {
     public DbSet<EmailTemplate> EmailTemplates => Set<EmailTemplate>();
 
     public DbSet<SmsTemplate> SmsTemplates => Set<SmsTemplate>();
-    
-    public NotificationDbContext(DbContextOptions<NotificationDbContext> options) : base(options)
-    {
-        
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("notification");
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(NotificationDbContext).Assembly);
+        modelBuilder.ApplyEntityConfigurations<NotificationDbContext>();
     }
 }
