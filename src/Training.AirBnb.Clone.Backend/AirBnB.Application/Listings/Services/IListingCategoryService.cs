e8a5af636c422b5cbc9cd78f2374fd892bd5a3cb
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-26 08:06:16 +0500

    Add IListingCategoryService interface

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Listings/Services/IListingCategoryService.cs
@@ -0,0 +1,20 @@
+﻿using AirBnB.Domain.Common.Query;
+using AirBnB.Domain.Entities.Listings;
+
+namespace AirBnB.Application.Listings.Services;
+
+/// <summary>
+/// Defines location category foundation service functionalities.
+/// </summary>
+public interface IListingCategoryService
+{
+    /// <summary>
+    /// Retrieves a list of locations categories based on the provided query specification.
+    /// </summary>
+    /// <param name="querySpecification"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<IList<ListingCategory>> GetAsync(
+        QuerySpecification<ListingCategory> querySpecification,
+        CancellationToken cancellationToken = default);
+}
