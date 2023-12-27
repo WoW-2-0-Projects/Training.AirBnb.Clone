582e7f0af49d9b8ab6c3c24642a9f1151504eac4
Jasurbek1223 <jasurabdulxaev@gmail.com>
2023-12-27 15:23:56 +0500

    comments on the AccessToken.cs and ClaimConstants.cs

--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Constants/ClaimConstants.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Constants/ClaimConstants.cs
@@ -1,7 +1,17 @@
 ﻿namespace AirBnB.Domain.Constants;
 
+/// <summary>
+/// Static class containing constants for standard claim names.
+/// </summary>
 public static class ClaimConstants
 {
+    /// <summary>
+    /// Constant representing the User ID claim name.
+    /// </summary>
     public const string UserId = "UserId";
+
+    /// <summary>
+    /// Constant representing the Access Token ID claim name.
+    /// </summary>
     public const string AccessTokenId = "AccessTokenId";
 }
