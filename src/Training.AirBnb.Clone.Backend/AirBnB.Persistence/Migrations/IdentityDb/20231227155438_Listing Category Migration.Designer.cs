993aea9e629f720daf8eb3aee6ec2ea3e906fc23
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-27 21:10:56 +0500

    Add Listing Category Migration

--- src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Migrations/IdentityDb/20231227155438_Listing Category Migration.Designer.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Migrations/IdentityDb/20231227155438_Listing Category Migration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AirBnB.Persistence.DataContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AirBnB.Persistence.Migrations.IdentityDb
 {
     [DbContext(typeof(IdentityDbContext))]
-    partial class IdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231227155438_Listing Category Migration")]
+    partial class ListingCategoryMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
