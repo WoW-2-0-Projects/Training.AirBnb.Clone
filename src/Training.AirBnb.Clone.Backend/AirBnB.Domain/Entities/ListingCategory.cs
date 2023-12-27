9a1a623d3bd9439d1520f384a4fc20b55862cc9d
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-27 20:26:58 +0500

    Add Listing Category entity

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Entities/ListingCategory.cs
@@ -0,0 +1,29 @@
+﻿using AirBnB.Domain.Common;
+
+namespace AirBnB.Domain.Entities;
+
+/// <summary>
+/// Represents a category for a listing
+/// </summary>
+public class ListingCategory : SoftDeletedEntity
+{
+    /// <summary>
+    /// Gets or sets the category name
+    /// </summary>
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the category is a special category or regular one.
+    /// </summary>
+    public bool IsSpecialCategory { get; set; }
+
+    /// <summary>
+    /// Gets or sets the category image id
+    /// </summary>
+    public Guid StorageFileId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the category image
+    /// </summary>
+    public virtual StorageFile ImageStorageFile { get; set; }
+}
