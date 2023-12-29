8833795d03e21457976d6b2292c4f8a7df418645
Ilxom <ilxomkarimjonov22@gmail.com>
2023-12-29 21:20:46 +0500

    Create Listings Container component and add it to Home View

--- src/Training.AirBnb.Clone.Frontend/src/common/views/HomeView.vue
+++ src/Training.AirBnb.Clone.Frontend/src/common/views/HomeView.vue
@@ -1,16 +1,24 @@
 <template>
 
-  <!-- Header-->
+    <!-- Header -->
   <main-header/>
 
+    <!-- Content -->
+    <article class="mt-[20px] content-padding">
 
+        <!-- Listings Container -->
+        <listings-container/>
+        
+    </article>
+      
   <button class="mt-80" @click="appThemeService.toggleDarkMode()">Dark mode</button>
-</template>
 
+</template>
 <script setup lang="ts">
 
 import MainHeader from "@/common/components/MainHeader.vue";
 import { AppThemeService } from "@/infrastructure/services/AppThemeService";
+    import ListingsContainer from "@/modules/locations/components/ListingsContainer.vue";
 
 const appThemeService = new AppThemeService();
 
