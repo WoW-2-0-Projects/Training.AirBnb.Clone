45cb49b2b8b3f6cc5d8bf99322cbc79230e023b8
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2024-01-17 13:24:42 +0500

    Add Notification Templates migration using TPH relation

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Migrations/20240117081731_Notification Templates Migration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirBnB.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotificationTemplatesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotificationTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Content = table.Column<string>(type: "character varying(129536)", maxLength: 129536, nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    TemplateType = table.Column<int>(type: "integer", nullable: false),
+                    Subject = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DeletedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ModifiedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationTemplates", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationTemplates_Type_TemplateType",
+                table: "NotificationTemplates",
+                columns: new[] { "Type", "TemplateType" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationTemplates");
+        }
+    }
+}
