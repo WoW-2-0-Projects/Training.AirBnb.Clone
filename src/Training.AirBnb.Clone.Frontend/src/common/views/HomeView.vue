559e866f1ab3b5ef2cae347cea6d1d436e0e8136
Ilxom <ilxomkarimjonov22@gmail.com>
2024-01-18 09:21:42 +0500

    Fix airbnb api request urls and refactor

--- src/Training.AirBnb.Clone.Frontend/src/common/views/HomeView.vue
+++ src/Training.AirBnb.Clone.Frontend/src/common/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div class="h-screen w-screen flex flex-col theme-bg-primary">
+<div class="flex flex-col w-screen h-screen theme-bg-primary">
 
   <!-- Header-->
   <main-header/>
@@ -22,7 +22,7 @@
 import ListingsGrid from "@/Modules/locations/components/ListingsGrid.vue";
 import MainHeader from "@/common/components/MainHeader.vue";
 import { AppThemeService } from "@/infrastructure/services/AppThemeService";
-    import ListingsContainer from "@/modules/locations/components/ListingsContainer.vue";
+import ListingsContainer from "@/modules/locations/components/ListingsContainer.vue";
 
 const appThemeService = new AppThemeService();
 
