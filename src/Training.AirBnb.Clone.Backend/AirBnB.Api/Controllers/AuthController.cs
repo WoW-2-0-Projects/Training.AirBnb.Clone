2ccf6df1741f12ca66523ce203a58473aeef5bf4
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-01-19 23:17:05 +0500

    Update AuthController

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using AirBnB.Application.Common.Identity.Models;
+using AirBnB.Application.Common.Identity.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirBnB.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController(IAuthService authService, IMapper mapper) : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegistrationDetails registrationDetails, CancellationToken cancellationToken)
+    {
+        var result = await authService.RegisterAsync(registrationDetails, cancellationToken);
+        return result ? Ok(result) : BadRequest();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDetails loginDetails, CancellationToken cancellationToken)
+    {
+        var result = await authService.LoginAsync(loginDetails);
+        return Ok(result);
+    }
+}
