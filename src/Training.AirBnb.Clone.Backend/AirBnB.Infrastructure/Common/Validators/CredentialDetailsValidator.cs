e0402d49bc3d8dd40a3a534ede8b95ac7df5df68
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-01-20 17:50:57 +0500

    Added CredentialDetailsvalidator file

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Validators/CredentialDetailsValidator.cs
@@ -0,0 +1,21 @@
+﻿using AirBnB.Application.Common.Identity.Models;
+using FluentValidation;
+
+namespace AirBnB.Infrastructure.Common.Validators;
+
+public class CredentialDetailsValidator : AbstractValidator<CredentialDetails>
+{
+    public CredentialDetailsValidator()
+    {
+        RuleFor(credential => credential.Password)
+            .Custom(
+                (password, context) =>
+                {
+                    if (context.RootContextData.TryGetValue("PersonalInformation", out var userInfoObj) &&
+                        userInfoObj is IEnumerable<string> userInfo &&
+                        userInfo.Any(info => !string.IsNullOrEmpty(info) && password.Contains(info)))
+                        context.AddFailure("Password must not contain user public information.");
+                }
+            );
+    }
+}
