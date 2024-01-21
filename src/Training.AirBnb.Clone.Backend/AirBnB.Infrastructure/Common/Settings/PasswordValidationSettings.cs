ee7a0daf97dfda8beb3aae919cc7eea83753b330
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-01-21 17:28:21 +0500

    Added PasswordValidationSettings

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Settings/PasswordValidationSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace AirBnB.Infrastructure.Common.Settings;
+
+public class PasswordValidationSettings
+{
+    public bool RequireDigit { get; set; }
+    public bool RequireUppercase { get; set; }
+    public bool RequireLowercase { get; set; }
+    public bool RequireNonAlphanumeric { get; set; }
+    public int MinimumLength { get; set; }
+    public int MaximumLength { get; set; }
+}
