9ef719ffe13e410deb34639701a395a080ba30bc
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-01-21 17:44:41 +0500

    Added CharExtensions

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Extension/CharExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace AirBnB.Domain.Extension;
+
+public static class CharExtensions
+{
+    public static char GetRandomDigit(Random? random = null)
+    {
+        return (char)(random ?? new Random()).Next('0', '9' + 1);
+    }
+
+    public static char GetRandomUppercase(Random? random = null)
+    {
+        return (char)(random ?? new Random()).Next('A', 'Z' + 1);
+    }
+
+    public static char GetRandomLowercase(Random? random = null)
+    {
+        return (char)(random ?? new Random()).Next('a', 'z' + 1);
+    }
+
+    public static char GetRandomNonAlphanumeric(Random? random = null)
+    {
+        return "!@#$%^&*()_-+=<>?"[(random ?? new Random()).Next(16)];
+    }
+
+    public static char GetRandomCharacter(Random? random)
+    {
+        return (char)(random ?? new Random()).Next(32, 126);
+    }
+}
