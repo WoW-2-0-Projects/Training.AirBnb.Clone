d9d6103c1685312b206959ef0fa11cc1df606a19
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-01-22 17:28:44 +0500

    Added UserRoleConfiguration

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/EntityConfigurations/UserRolesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AirBnB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AirBnB.Persistence.EntityConfigurations;
+
+public class UserRolesConfiguration : IEntityTypeConfiguration<UserRole>
+{
+    public void Configure(EntityTypeBuilder<UserRole> builder)
+    {
+        builder.HasKey(userRole => new { userRole.UserId, userRole.RoleId });
+
+        builder.HasOne<Role>(userRole => userRole.Role)
+            .WithMany(role => role.Users)
+            .HasForeignKey(userRole => userRole.RoleId);
+        
+        builder.HasOne<User>(userRole => userRole.User)
+            .WithMany(user => user.Roles)
+            .HasForeignKey(userRoles => userRoles.UserId);
+    }
+}
