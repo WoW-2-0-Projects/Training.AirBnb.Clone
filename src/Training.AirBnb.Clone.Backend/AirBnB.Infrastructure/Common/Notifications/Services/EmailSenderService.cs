982612d9bc5272c4a61bac2467a15e9b90e44f33
Mirzohid-Mominov <momirzohid4@gmail.com>
2024-01-24 11:35:33 +0500

    Created SmsSenderService and EmailSenderService by using their interfaces

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Notifications/Services/EmailSenderService.cs
@@ -0,0 +1,59 @@
+﻿using AirBnB.Application.Common.Notifications.Brokers;
+using AirBnB.Application.Common.Notifications.Models;
+using AirBnB.Application.Common.Notifications.Services;
+using AirBnB.Domain.Enums;
+using AirBnB.Domain.Extensions;
+using FluentValidation;
+
+namespace AirBnB.Infrastructure.Common.Notifications.Services;
+/// <summary>
+/// Implementation of the IEmailSenderService interface orchestrating the sending of email messages.
+/// </summary>
+public class EmailSenderService : IEmailSenderService
+{
+    private readonly IEnumerable<IEmailSenderBroker> _emailSenderBrokers;
+    private readonly IValidator<EmailMessage> _emailMessageValidator;
+    
+    /// <summary>
+    /// Initializes a new instance of the EmailSenderService class.
+    /// </summary>
+    /// <param name="emailSenderBroker">Collection of email sender brokers injected via dependency injection.</param>
+    /// <param name="emailMessageValidator">Validator for email messages injected via dependency injection.</param>
+    public EmailSenderService(
+        IEnumerable<IEmailSenderBroker> emailSenderBroker,
+        IValidator<EmailMessage> emailMessageValidator)
+    {
+        _emailSenderBrokers = emailSenderBroker;
+        _emailMessageValidator = emailMessageValidator;
+    }
+    
+    /// <summary>
+    /// Sends an email asynchronously by orchestrating the process through available EmailSenderBrokers.
+    /// </summary>
+    /// <param name="emailMessage">The EmailMessage object representing the email to be sent.</param>
+    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the operation to complete.</param>
+    /// <returns>
+    /// A ValueTask;bool; representing the asynchronous operation's success status.
+    /// </returns>
+    public async ValueTask<bool> SendAsync(EmailMessage emailMessage, CancellationToken cancellationToken = default)
+    {
+            var validationResult = _emailMessageValidator.Validate(emailMessage,
+                options => options.IncludeRuleSets(NotificationEvent.OnSending.ToString()));
+
+            if (!validationResult.IsValid)
+                throw new ValidationException(validationResult.Errors);
+
+            foreach (var smsSenderBroker in _emailSenderBrokers)
+            {
+                var sendNotifcationTask = () => smsSenderBroker.SendAsync(emailMessage, cancellationToken);
+                var result = await sendNotifcationTask.GetValueAsync();
+
+                emailMessage.IsSuccessful = result.IsSuccess;
+                emailMessage.ErrorMessage = result.Exception.Message;
+
+                return result.IsSuccess;
+            }
+            
+            return false;
+    }
+}
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Notifications/Services/SmsSenderService.cs
@@ -0,0 +1,58 @@
+﻿using AirBnB.Application.Common.Notifications.Brokers;
+using AirBnB.Application.Common.Notifications.Models;
+using AirBnB.Application.Common.Notifications.Services;
+using AirBnB.Domain.Enums;
+using AirBnB.Domain.Extensions;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace AirBnB.Infrastructure.Common.Notifications.Services;
+
+/// <summary>
+/// Implementation of the ISmsSenderService interface orchestrating the sending of SMS messages.
+/// </summary>
+public class SmsSenderService : ISmsSenderService
+{
+    private readonly IEnumerable<ISmsSenderBroker> _smsSenderBrokers;
+    private readonly IValidator<SmsMessage> _smsMessageValidators;
+
+    /// <summary>
+    /// Initializes a new instance of the SmsSenderService class.
+    /// </summary>
+    /// <param name="smsSenderBrokers">Collection of SMS sender brokers injected via dependency injection.</param>
+    /// <param name="smsMessageValidator">Validator for SMS messages injected via dependency injection.</param>
+    public SmsSenderService(
+        IEnumerable<ISmsSenderBroker> smsSenderBrokers,
+        IValidator<SmsMessage> smsMessageValidator)
+    {
+        _smsSenderBrokers = smsSenderBrokers;
+        _smsMessageValidators = smsMessageValidator;
+    }
+    
+    /// <summary>
+    /// Sends an SMS asynchronously by orchestrating the process through available SmsSenderBrokers.
+    /// </summary>
+    /// <param name="smsMessage">The SmsMessage object representing the SMS to be sent.</param>
+    /// <param name="cancellationToken">A CancellationToken to observe while waiting for the operation to complete.</param>
+    /// <returns>A ValueTask&lt;bool&gt; representing the asynchronous operation's success status.</returns>
+    public async ValueTask<bool> SendAsync(SmsMessage smsMessage, CancellationToken cancellationToken = default)
+    {
+        var validationResult = _smsMessageValidators.Validate(smsMessage,
+            options => options.IncludeRuleSets(NotificationEvent.OnRendering.ToString()));
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        foreach (var smsSenderBroker in _smsSenderBrokers)
+        {
+            var sendNotificationTask = () => smsSenderBroker.SendAsync(smsMessage, cancellationToken);
+            var result = await sendNotificationTask.GetValueAsync();
+
+            smsMessage.IsSuccessful = result.IsSuccess;
+            smsMessage.ErrorMessage = result.Exception.Message;
+            return result.IsSuccess;
+        }
+
+        return false;
+    }
+}
