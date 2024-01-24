afc964d66f439b4aee1fd697d343984bb7c1f433
Bunyod <kbunyod3011@gmail.com>
2024-01-24 21:57:12 +0500

    Add ICommand and ICommandHandler interfaces

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Commands/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace AirBnB.Domain.Commands;
+
+public interface ICommand<out TResult> : ICommand, IRequest<TResult>
+{
+    
+}
+
+public interface ICommand
+{
+    
+}
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Commands/ICommandHandler.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace AirBnB.Domain.Commands;
+
+
+public interface ICommandHandler<in TCommand, TResult> : IRequestHandler<TCommand, TResult> where TCommand : ICommand<TResult>
+{
+}
