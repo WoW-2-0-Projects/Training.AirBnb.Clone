59a2d31b2f9b559b13ca40b458803c0b6313d704
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-01-25 15:19:16 +0500

    Added IRoleService and RoleService

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Identity/Services/IUserRoleService.cs
@@ -0,0 +1,16 @@
+﻿using AirBnB.Domain.Entities;
+using AirBnB.Domain.Enums;
+
+namespace AirBnB.Application.Common.Identity.Services;
+
+public interface IUserRoleService
+{
+    /// <summary>
+    /// Revokes a specified role from a user asynchronously.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user whose role is being revoked</param>
+    /// <param name="roleType">The type of role to revoke.</param>
+    /// <param name="cancellationToken"> A cancellation token that can be used to cancel the operation.</param>
+    /// <returns>A task that will complete with a value indicating whether the role was successfully revoked</returns>
+    ValueTask<bool> RevokeRoleAsync(Guid userId, RoleType roleType, CancellationToken cancellationToken = default);
+}
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Identity/Services/UserRoleService.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Authentication;
+using AirBnB.Application.Common.Identity.Services;
+using AirBnB.Domain.Enums;
+using AirBnB.Persistence.DataContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirBnB.Infrastructure.Common.Identity.Services;
+
+/// <summary>
+/// Provides services for managing user roles within the application.
+/// </summary>
+/// <param name="appDbContext"></param>
+/// <param name="userService"></param>
+public class UserRoleService(
+    AppDbContext appDbContext, 
+    IUserService userService
+    ) : IUserRoleService
+{
+    public async ValueTask<bool> RevokeRoleAsync(Guid userId, RoleType roleType, CancellationToken cancellationToken = default)
+    {
+        //bring the user all his materials from the database by email address entered
+        var user = await userService.Get(asNoTracking: true)
+            .Include(user => user.Roles)
+            .ThenInclude(role => role.Role)
+            .FirstOrDefaultAsync(user => user.Id == userId,
+                cancellationToken: cancellationToken) ?? throw new InvalidOperationException("User not found");
+        
+        //choosing the right role
+        var selectedRole = user.Roles.FirstOrDefault(role => role.Role.Type == roleType) ?? throw new AuthenticationException(" Invalid role type");
+        
+        //delete the selected role
+        appDbContext.UserRoles.Remove(selectedRole);
+        
+        await appDbContext.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+}
