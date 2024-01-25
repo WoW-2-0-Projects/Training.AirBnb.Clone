8c65b9efc2ae41af7f341cd3ad6b3a4e44aa2cac
Mirzohid-Mominov <momirzohid4@gmail.com>
2024-01-25 16:20:30 +0500

    Created UserCredentials model with PasswordHash property

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Entities/UserCredentials.cs
@@ -0,0 +1,14 @@
+﻿using AirBnB.Domain.Common.Entities;
+
+namespace AirBnB.Domain.Entities;
+
+/// <summary>
+/// Represents user credentials with a hashed password.
+/// </summary>
+public class UserCredentials
+{
+    /// <summary>
+    /// Gets or sets hashed password for the user.
+    /// </summary>
+    public string PasswordHash { get; set; } = default!;
+}
