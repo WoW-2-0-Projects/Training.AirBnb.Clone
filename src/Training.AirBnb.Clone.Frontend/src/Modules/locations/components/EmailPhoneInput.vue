9215c191b04bf3c1c463ae7b89203b7ab33b6645
Ilxom <ilxomkarimjonov22@gmail.com>
2024-01-25 21:58:38 +0500

    Add email phone input component

---
+++ src/Training.AirBnb.Clone.Frontend/src/Modules/locations/components/EmailPhoneInput.vue
@@ -0,0 +1,52 @@
+<template>
+
+<div class="mt-5">
+    <CountryCodePicker v-if="!byEmail"/>
+
+    <div class="h-[56px] w-[520px] flex flex-col justify-center px-3 py-1"
+        :class="[isFocused ? 'border-2 rounded-md border-black' : 'border rounded-md border-[#b0b0b0]']"
+        tabindex="0"
+        @click="toggleIsFocused"
+        @blur="onBlur">
+        
+        <h3 class="text-[#797979] text-[12px]">{{ byEmail ? 'Email' : 'Phone number' }}</h3>
+        <input v-if="!byEmail" type="text" class="text-base text-textPrimary focus:outline-none" @keydown="handleKeyDown">
+        <input v-if="byEmail" type="text" class="text-base text-textPrimary focus:outline-none">
+    </div>
+</div>
+
+
+</template>
+
+<script setup lang="ts">
+
+import CountryCodePicker from './CountryCodePicker.vue';
+import { ref } from 'vue';
+
+const props = defineProps({
+    byEmail: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const isFocused = ref<boolean>(false);
+
+const toggleIsFocused = () => {
+    isFocused.value = !isFocused.value;
+}
+
+const onBlur = () => {
+    isFocused.value = false;
+}
+
+
+const handleKeyDown = (event: KeyboardEvent) => {
+    const allowedKeys = ['Backspace', 'ArrowRight', 'ArrowUp', 'ArrowDown']
+
+    if (!allowedKeys.includes(event.key) && (event.key.length === 1 && isNaN(Number(event.key)))) {
+        event.preventDefault();
+      }
+}
+
+</script>
