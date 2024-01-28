4780ec404724420a00f71f7780987d015d9b8d21
Bunyod <kbunyod3011@gmail.com>
2024-01-29 02:13:30 +0500

    Add RabbitMQConnectionProvider and RabbitMqBusBroker

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/EventBus/Brokers/RabbitMqConnectionProvider.cs
@@ -0,0 +1,32 @@
+﻿using AirBnB.Application.Common.EventBus.Brokers;
+using AirBnB.Infrastructure.Common.Settings;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+
+namespace AirBnB.Infrastructure.Common.EventBus.Brokers;
+
+public class RabbitMqConnectionProvider : IRabbitMqConnectionProvider
+{
+    private ConnectionFactory _connectionFactory;
+
+    private IConnection? _connection;
+
+    public RabbitMqConnectionProvider(IOptions<RabbitMqConnectionSettings> rabbitMqConnectionSettings)
+    {
+        var rabbitMqConnectionSettings1 = rabbitMqConnectionSettings.Value;
+
+        _connectionFactory = new ConnectionFactory()
+        {
+            HostName = rabbitMqConnectionSettings1.HostName,
+            Port =  rabbitMqConnectionSettings1.Port
+        };
+
+    }
+    
+    public async ValueTask<IChannel> CreateChannelAsync()
+    {
+        _connection ??= await _connectionFactory.CreateConnectionAsync();
+
+        return await _connection.CreateChannelAsync();
+    }
+}
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/EventBus/Brokers/RabbitMqEventBusBroker.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using AirBnB.Application.Common.EventBus.Brokers;
+using AirBnB.Application.Common.Serializers;
+using AirBnB.Domain.Common.Events;
+using AirBnB.Domain.Events;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using RabbitMQ.Client;
+
+namespace AirBnB.Infrastructure.Common.EventBus.Brokers;
+
+public class RabbitMqEventBusBroker(
+    IRabbitMqConnectionProvider rabbitMqConnectionProvider,
+    IJsonSerializationSettingsProvider jsonSerializationSettingsProvider
+    ) : IEvenBusBroker
+{
+    public async ValueTask PublishAsync<TEvent>(TEvent @event, string exchange, string routingKey, CancellationToken cancellationToken) where TEvent : Event
+    {
+        var channel = await rabbitMqConnectionProvider.CreateChannelAsync();
+
+        var properties = new BasicProperties()
+        {
+            Persistent = true
+        };
+
+        var serializerSettings = jsonSerializationSettingsProvider.Get(true);
+        serializerSettings.ContractResolver = new DefaultContractResolver();
+        serializerSettings.TypeNameHandling = TypeNameHandling.All;
+
+        var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event, serializerSettings));
+        await channel.BasicPublishAsync(exchange, routingKey, properties, body);
+    }
+}
