7571207af066345b08abfbfec022ec10e4046fbc
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-01-31 16:15:28 +0500

    Fix delete auditable entities migration

--- src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Persistence/Migrations/20240131111451_UpdateDeleteAuditableEntities.Designer.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Persistence/Migrations/20240131111451_UpdateDeleteAuditableEntities.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using AirBnB.Persistence.DataContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace AirBnB.Persistence.Migrations
+namespace AirBnB.Persistence.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240131111451_UpdateDeleteAuditableEntities")]
+    partial class UpdateDeleteAuditableEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
