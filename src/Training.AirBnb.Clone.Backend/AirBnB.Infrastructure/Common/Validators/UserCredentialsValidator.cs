fff28efccb245619a19956f368f83b5e04003015
Mirzohid-Mominov <momirzohid4@gmail.com>
2024-01-31 12:46:19 +0500

    Created UserCredentialsValidator service

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Validators/UserCredentialsValidator.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AirBnB.Application.Common.Settings;
+using AirBnB.Domain.Entities;
+using AirBnB.Domain.Enums;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace AirBnB.Infrastructure.Common.Validators;
+
+public class UserCredentialsValidator : AbstractValidator<UserCredentials>
+{
+    public UserCredentialsValidator(IOptions<ValidationSettings> validationSettings)
+    {
+        var validationSettingsValue = validationSettings.Value;
+        
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(userCredentials => userCredentials.PasswordHash)
+                    .NotEmpty()
+                    .MinimumLength(8)
+                    .MaximumLength(64)
+                    .Matches(validationSettingsValue.PasswordRegexPattern);
+            });
+    }
+}
