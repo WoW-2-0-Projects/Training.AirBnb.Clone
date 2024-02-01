b81ef2eb2febcb5523d80744d655235fdb1234bd
Azizbek Abdurahmonov <azizbek.abdura52@gmail.com>
2024-02-01 01:45:51 -0800

    Sms/Email history repositories added

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Repositories/EmailHistoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using AirBnB.Persistence.Caching.Brokers;
+using AirBnB.Persistence.Caching.Models;
+using AirBnB.Persistence.DataContexts;
+using AirBnB.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace AirBnB.Persistence.Repositories;
+
+public class EmailHistoryRepository(AppDbContext dbContext, ICacheBroker cacheBroker)
+    : EntityRepositoryBase<EmailHistory, AppDbContext>(dbContext, cacheBroker, new CacheEntryOptions()), ISmsHistoryRepository
+{
+    public new IQueryable<EmailHistory> Get(
+        Expression<Func<EmailHistory, bool>>? predicate = null,
+        bool asNoTracking = false) => base.Get(predicate, asNoTracking);
+
+    public new ValueTask<EmailHistory> CreateAsync(EmailHistory emailHistory, bool saveChanges = true, CancellationToken cancellationToken = default)
+        => base.CreateAsync(emailHistory, saveChanges, cancellationToken);
+}
