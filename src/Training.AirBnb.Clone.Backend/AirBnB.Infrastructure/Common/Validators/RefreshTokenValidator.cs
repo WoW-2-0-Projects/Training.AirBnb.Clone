308efdba522d135129c1ec000a89372a3cd6dab6
Habiba0206 <habibasattorova26@gmail.com>
2024-02-13 20:32:03 +0500

    Created refresh token entity, its repository and validator

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Infrastructure/Common/Validators/RefreshTokenValidator.cs
@@ -0,0 +1,51 @@
+﻿using AirBnB.Domain.Enums;
+using AirBnB.Infrastructure.Common.Settings;
+using Microsoft.Extensions.Options;
+
+namespace AirBnB.Infrastructure.Common.Validators;
+
+public class RefreshTokenValidator
+{
+    public RefreshTokenValidator(IOptions<JwtSettings> jwtSettings)
+    {
+        var identityTokenSettingsValue = jwtSettings.Value;
+
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(refreshToken => refreshToken.Id).NotEqual(Guid.Empty);
+
+                RuleFor(refreshToken => refreshToken.Token).NotEmpty();
+
+                RuleFor(refreshToken => refreshToken.UserId).NotEqual(Guid.Empty);
+
+                RuleFor(refreshToken => refreshToken.ExpiryTime)
+                    .GreaterThan(DateTimeOffset.UtcNow)
+                    .Custom(
+                        (refreshToken, context) =>
+                        {
+                            if (refreshToken >
+                                DateTimeOffset.UtcNow.AddMinutes(identityTokenSettingsValue.RefreshTokenExtendedExpirationTimeInMinutes))
+                                context.AddFailure(
+                                    nameof(RefreshToken.ExpiryTime),
+                                    $"{nameof(RefreshToken.ExpiryTime)} cannot be greater than the expiration time of the JWT token."
+                                );
+                        }
+                    )
+                    .When(refreshToken => refreshToken.EnableExtendedExpiryTime)
+                    .Custom(
+                        (refreshToken, context) =>
+                        {
+                            if (refreshToken > DateTimeOffset.UtcNow.AddMinutes(identityTokenSettingsValue.RefreshTokenExpirationTimeInMinutes))
+                                context.AddFailure(
+                                    nameof(RefreshToken.ExpiryTime),
+                                    $"{nameof(RefreshToken.ExpiryTime)} cannot be greater than the expiration time of the JWT token."
+                                );
+                        }
+                    )
+                    .When(refreshToken => !refreshToken.EnableExtendedExpiryTime);
+            }
+        );
+    }
+}
