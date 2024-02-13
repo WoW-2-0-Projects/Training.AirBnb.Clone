73ad7d16c626804d13cd8d7ea47b427989894626
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-02-13 20:39:52 +0500

    added some methods to identity security token services

--- src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Identity/Services/IIdentitySecurityTokenGeneratorService.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Identity/Services/IIdentitySecurityTokenGeneratorService.cs
@@ -7,21 +7,36 @@ namespace AirBnB.Application.Common.Notifications.Services;
 /// <summary>
 /// Interface for a service responsible for generating and managing access tokens.
 /// </summary>
-public interface IAccessTokenGeneratorService
+public interface IIdentitySecurityTokenGeneratorService
 {
     /// <summary>
     /// Generates an access token for the specified user.
     /// </summary>
     /// <param name="user">The user for whom the access token is generated.</param>
     /// <returns>An AccessToken object representing the generated access token.</returns>
-    AccessToken GetToken(User user);
+    AccessToken GenerateAccessToken(User user);
+
+    /// <summary>
+    /// Generates refresh token for the given user
+    /// </summary>
+    /// <param name="user">User to create refresh token for</param>
+    /// <param name="extendedExpiryTime">Indicates if its expiry time should be expired</param>
+    /// <returns>RefReshToken object representing the generated refresh token</returns>
+    RefreshToken GenerateRefreshToken(User user, bool extendedExpiryTime = false);
+
+    /// <summary>
+    /// Gets claims principal from given token value
+    /// </summary>
+    /// <param name="tokenValue">Valid access token value</param>
+    /// <returns>Claims principal</returns>
+    AccessToken? GetAccessToken(string tokenValue);
 
     /// <summary>
     /// Retrieves the unique identifier (ID) associated with the provided access token.
     /// </summary>
     /// <param name="accessToken">The access token for which the ID is to be retrieved.</param>
     /// <returns>The unique identifier (ID) associated with the provided access token.</returns>
-    Guid GetTokenId(string accessToken);
+    Guid GetAccessTokenId(string accessToken);
 
     /// <summary>
     /// Generates a JWT security token for the specified user and access token.
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Application/Common/Identity/Services/IIdentitySecurityTokenService.cs
@@ -0,0 +1,73 @@
+﻿using AirBnB.Domain.Entities;
+
+namespace AirBnB.Application.Common.Identity.Services;
+
+/// <summary>
+/// Interface for managing access tokens.
+/// </summary>
+public interface IIdentitySecurityTokenService
+{
+    /// <summary>
+    /// Asynchronously creates a new access token.
+    /// </summary>
+    /// <param name="accessToken">The access token to create.</param>
+    /// <param name="saveChanges">Indicates whether changes should be saved to the underlying data store (default is true).</param>
+    /// <param name="cancellationToken">Cancellation token to stop the operation if needed (default is none).</param>
+    /// <returns>A ValueTask representing the asynchronous operation, returning the created AccessToken.</returns>
+    ValueTask<AccessToken> CreateAccessTokenAsync(
+        AccessToken accessToken, 
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously retrieves an access token by its unique identifier.
+    /// </summary>
+    /// <param name="accessTokenId">The unique identifier of the access token to retrieve.</param>
+    /// <param name="cancellationToken">Cancellation token to stop the operation if needed (default is none).</param>
+    /// <returns>A ValueTask representing the asynchronous operation, returning the retrieved AccessToken, or null if not found.</returns>
+    ValueTask<AccessToken?> GetAccessTokenByIdAsync(
+        Guid accessTokenId, 
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously creats refresh token for given user
+    /// </summary>
+    /// <param name="refreshToken">The refresh token to create</param>
+    /// <param name="saveChanges">Indicates whether changes should be saved to the underlying data store (default is true)</param>
+    /// <param name="cancellationToken">Cancellation token to stop the operation if needed (default is none)</param>
+    /// <returns>A ValueTask representing the asynchronous operation, returning the created RefreshToken</returns>
+    ValueTask<RefreshToken> CreateRefreshTokenAsync(
+        RefreshToken refreshToken,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously gets refresh token by given token value
+    /// </summary>
+    /// <param name="refreshTokenValue">The unique token value of the refresh token to get</param>
+    /// <param name="cancellationToken">Cancellation token to stop the operation if needed (default is none)param>
+    /// <returns>A ValueTask representing the asynchronous operation, returning the retrieved RefreshToken, or null if not found</returns>
+    ValueTask<RefreshToken?> GetRefreshTokenByValueAsync(
+        string refreshTokenValue, 
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously revokes (invalidates) an access token.
+    /// </summary>
+    /// <param name="accessTokenId">The unique identifier of the access token to revoke.</param>
+    /// <param name="cancellationToken">Cancellation token to stop the operation if needed (default is none).</param>
+    /// <returns>A ValueTask representing the asynchronous operation.</returns>
+    ValueTask RevokeAccessTokenAsync(
+        Guid accessTokenId, 
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously removes refresh token
+    /// </summary>
+    /// <param name="refreshTokenValue">The unique token value of the refresh token to get</param>
+    /// <param name="cancellationToken">Cancellation token to stop the operation if needed (default is none)</param>
+    /// <returns>A ValueTask representing the asynchronous operation</returns>
+    ValueTask RemoveRefreshTokenAsync(
+        string refreshTokenValue, 
+        CancellationToken cancellationToken = default);
+}
