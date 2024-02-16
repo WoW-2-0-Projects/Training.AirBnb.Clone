9bd5e2063335e400f1d4ddb55bb66591a7df4c06
Ilxom <ilxomkarimjonov22@gmail.com>
2024-02-16 22:17:44 +0500

    Add notification template and event bus constants

---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Constants/EventBusConstants.cs
@@ -0,0 +1,27 @@
+﻿namespace AirBnB.Domain.Constants;
+
+/// <summary>
+/// Constants related to the event bus configuration for handling notifications.
+/// </summary>
+public static class EventBusConstants
+{
+    /// <summary>
+    /// The exchange name for routing notifications within the event bus.
+    /// </summary>
+    public const string NotificationExchangeName = "Notifications";
+    
+    /// <summary>
+    /// The queue name for processing notifications within the event bus.
+    /// </summary>
+    public const string ProcessNotificationQueueName = "ProcessNotification";
+    
+    /// <summary>
+    /// The queue name for rendering notifications within the event bus.
+    /// </summary>
+    public const string RenderNotificationQueueName = "RenderNotification";
+    
+    /// <summary>
+    /// The queue name for sending notifications within the event bus.
+    /// </summary>
+    public const string SendNotificationQueueName = "SendNotification";
+}
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Constants/NotificationTemplateConstants.cs
@@ -0,0 +1,22 @@
+namespace AirBnB.Domain.Constants;
+
+/// <summary>
+/// Constants for placeholders used in notification templates.
+/// </summary>
+public static class NotificationTemplateConstants
+{
+    /// <summary>
+    /// Placeholder for the user's name in notification templates.
+    /// </summary>
+    public const string UserNamePlaceholder = "UserName";
+
+    /// <summary>
+    /// Placeholder for the sender's name in notification templates.
+    /// </summary>
+    public const string SenderNamePlaceholder = "SenderName";
+    
+    /// <summary>
+    /// Placeholder for the verification link in email address verification notification templates.
+    /// </summary>
+    public const string EmailAddressVerificationLinkPlaceholder = "EmailAddressVerificationLink";
+}
