14cb13b68a2e515501a293b7f69810218fcef703
Ilxom <ilxomkarimjonov22@gmail.com>
2024-02-17 22:45:07 +0500

    Add comments to events

--- src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/Events/Event.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Domain/Common/Events/Event.cs
@@ -1,5 +1,8 @@
 namespace AirBnB.Domain.Common.Events;
 
+/// <summary>
+/// Represents an implementation of the IEvent interface, defining the properties for an event.
+/// </summary>
 public class Event : IEvent
 {
     public Guid Id { get; set; } = Guid.NewGuid();
