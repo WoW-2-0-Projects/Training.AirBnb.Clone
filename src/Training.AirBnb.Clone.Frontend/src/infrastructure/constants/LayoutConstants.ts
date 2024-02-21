93cc194d28435f433e3832e020caa6cf710586b7
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-02-21 12:50:52 +0500

    Add layout constants

---
+++ src/Training.AirBnb.Clone.Frontend/src/infrastructure/constants/LayoutConstants.ts
@@ -0,0 +1,9 @@
+export class LayoutConstants {
+    public static AirBnbYourHome = 'AirBnb Your Home';
+    public static SwitchToHostAccount = 'Switch to Host';
+
+
+
+    public static WelcomeToMessage = 'Welcome to Airbnb';
+    public static WelcomeBackMessage = 'Welcome back, ';
+}
