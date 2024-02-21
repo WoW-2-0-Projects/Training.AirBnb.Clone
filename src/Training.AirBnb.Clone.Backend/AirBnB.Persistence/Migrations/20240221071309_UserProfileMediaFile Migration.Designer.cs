ec92bde60c6cc21beb6dcb57b73cc279fe48a59e
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2024-02-21 14:02:04 +0500

    Add UserProfileMediaFiles Migration

--- src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Migrations/20240221071309_UserProfileMediaFile Migration.Designer.cs
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Migrations/20240221071309_UserProfileMediaFile Migration.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using AirBnB.Persistence.DataContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AirBnB.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240221071309_UserProfileMediaFile Migration")]
+    partial class UserProfileMediaFileMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Training.AirBnb.Clone.Backend/AirBnB.Persistence/Migrations/20240221071309_UserProfileMediaFile Migration.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirBnB.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserProfileMediaFileMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserProfileMediaFiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StorageFileId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfileMediaFiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserProfileMediaFiles_StorageFiles_StorageFileId",
+                        column: x => x.StorageFileId,
+                        principalTable: "StorageFiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserProfileMediaFiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfileMediaFiles_StorageFileId",
+                table: "UserProfileMediaFiles",
+                column: "StorageFileId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfileMediaFiles_UserId",
+                table: "UserProfileMediaFiles",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserProfileMediaFiles");
+        }
+    }
+}
